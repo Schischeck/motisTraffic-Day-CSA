c486924eaa4d9384f8a28eb338a2383ff770f8f9
Nikolay Semenov <semenov@ptu.tu-darmstadt.de>
2015-05-08 08:54:23 +0200

    one more try to resole name conflict

--- src/execution/websocketservice.cpp
+++ src/execution/websocketservice.cpp
@@ -1,46 +1,98 @@
-#include "websocketservice.h"
-
+#include <set>
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 
 namespace td {
 namespace execution {
+typedef websocketpp::server<websocketpp::config::asio> websocketserver;
 
+using websocketpp::connection_hdl;
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
 
-websocketservice::websocketservice() :  m_count(0) {
-    m_server.init_asio();
+class websocketservice {
+public:
+    websocketservice() {
+        m_server.init_asio();
 
-    m_server.set_open_handler(bind(&websocketservice::on_open,this,_1));
-    m_server.set_close_handler(bind(&websocketservice::on_close,this,_1));
+        m_server.set_open_handler(bind(&websocketservice::on_open, this, ::_1));
+        m_server.set_close_handler(bind(&websocketservice::on_close, this, ::_1));
+        m_server.set_message_handler(bind(&websocketservice::on_message, this, ::_1, ::_2));
+    }
+
+    void on_open(connection_hdl hdl) {
+        m_connections.insert(hdl);
+    }
+
+    void on_close(connection_hdl hdl) {
+        m_connections.erase(hdl);
+    }
+
+    void on_message(connection_hdl hdl, websocketserver::message_ptr msg) {
+        for (auto it : m_connections) {
+            m_server.send(it,msg);
+        }
+    }
+
+    void run(uint16_t port) {
+        m_server.listen(port);
+        m_server.start_accept();
+        m_server.run();
+    }
+
+private:
+    typedef std::set<connection_hdl, std::owner_less<connection_hdl>> con_list;
+
+    websocketserver m_server;
+    con_list m_connections;
+};
+}
 }
 
-void websocketservice::on_open(connection_hdl hdl) {
-    std::lock_guard<std::mutex> lock(m_mutex);
+//#include "websocketservice.h"
+
+
+//namespace td {
+//namespace execution {
+
+
+//websocketservice::websocketservice() :  m_count(0) {
+//    m_server.init_asio();
+
+//    m_server.set_open_handler(bind(&websocketservice::on_open,this,_1));
+//    m_server.set_close_handler(bind(&websocketservice::on_close,this,_1));
+//}
+
+//void websocketservice::on_open(connection_hdl hdl) {
+//    std::lock_guard<std::mutex> lock(m_mutex);
 //    m_connections.insert(hdl);
-}
+//}
 
-void websocketservice::on_close(connection_hdl hdl) {
-    std::lock_guard<std::mutex> lock(m_mutex);
+//void websocketservice::on_close(connection_hdl hdl) {
+//    std::lock_guard<std::mutex> lock(m_mutex);
 //    m_connections.erase(hdl);
-}
+//}
 
-void websocketservice::count() {
-    while (true) {
-        sleep(1);
-        m_count++;
+//void websocketservice::count() {
+//    while (true) {
+//        sleep(1);
+//        m_count++;
 
-        std::stringstream ss;
-        ss << m_count;
+//        std::stringstream ss;
+//        ss << m_count;
 
-        std::lock_guard<std::mutex> lock(m_mutex);
+//        std::lock_guard<std::mutex> lock(m_mutex);
 //        for (auto it : m_connections) {
 //            m_server.send(it,ss.str(),websocketpp::frame::opcode::text);
 //        }
-    }
-}
+//    }
+//}
 
-void websocketservice::run(uint16_t port) {
-    m_server.listen(port);
-    m_server.start_accept();
-    m_server.run();
-}
-}   // namespace execution
-}   // namespace td
+//void websocketservice::run(uint16_t port) {
+//    m_server.listen(port);
+//    m_server.start_accept();
+//    m_server.run();
+//}
+//}   // namespace execution
+//}   // namespace td
--- src/execution/websocketservice.h
+++ src/execution/websocketservice.h
@@ -1,41 +1,41 @@
 #ifndef WEBSOCKETSERVICE_H
 #define WEBSOCKETSERVICE_H
 
-#include <functional>
-#include <mutex>
-#include <set>
-#include <thread>
+//#include <functional>
+//#include <mutex>
+//#include <set>
+//#include <thread>
 
-#include <websocketpp/config/asio_no_tls.hpp>
-#include <websocketpp/server.hpp>
+//#include <websocketpp/config/asio_no_tls.hpp>
+//#include <websocketpp/server.hpp>
 
-typedef websocketpp::server<websocketpp::config::asio> websocketsrv;
+//typedef websocketpp::server<websocketpp::config::asio> websocketsrv;
 
-using websocketpp::connection_hdl;
+//using websocketpp::connection_hdl;
 
-namespace td {
+//namespace td {
 
-namespace execution {
+//namespace execution {
 
-class websocketservice {
-public:
-    websocketservice();
-    ~websocketservice();
+//class websocketservice {
+//public:
+//    websocketservice();
+//    ~websocketservice();
 
-    void on_open(websocketpp::connection_hdl hd1);
-    void on_close(websocketpp::connection_hdl hd1);
-    void count();
-    void run(uint16_t port);
-private:
-    typedef std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> con_list;
+//    void on_open(websocketpp::connection_hdl hd1);
+//    void on_close(websocketpp::connection_hdl hd1);
+//    void count();
+//    void run(uint16_t port);
+//private:
+//    typedef std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> con_list;
 
-    int m_count;
-    websocketsrv m_server;
-//    con_list m_connections;
-    std::mutex m_mutex;
-};
+//    int m_count;
+//    websocketsrv m_server;
+////    con_list m_connections;
+//    std::mutex m_mutex;
+//};
 
-}   // namespace execution
-}   // namespace td
+//}   // namespace execution
+//}   // namespace td
 
 #endif // WEBSOCKETSERVICE_H
