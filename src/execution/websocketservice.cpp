7a78cfdeb9fa504510127968719d66b2304040a6
Lars <larslotter@googlemail.com>
2015-05-09 01:44:06 +0200

    debug output removed

--- src/execution/websocketservice.cpp
+++ src/execution/websocketservice.cpp
@@ -51,20 +51,15 @@ void websocketservice::on_message(connection_hdl hdl, websocketserver::message_p
         for (long unsigned int i=0; i<this->m_stations.size(); i++) {
 
             tmp_station = m_stations[i].get();
-            std::cout << "tmp_station got" << std::endl;
-            if( tmp_station != NULL )
-                std::cout << "station not null" << std::endl;
             str_resp.append(std::to_string(i));
             str_resp.append(",");
             str_resp.append(std::to_string(tmp_station->length));
             str_resp.append(",");
             str_resp.append(std::to_string(tmp_station->width));
             str_resp.append(";");
-            std::cout << "appendet " << std::to_string(i) << "," << std::to_string(tmp_station->length) << std::endl;
-
             //std::cout << i << std::endl;
         }
-        std::cout << "sent stations" << std::endl;
+        //std::cout << "sent stations" << std::endl;
         std::cout << str_req << std::endl;
         m_server.send(hdl, str_resp, websocketpp::frame::opcode::text);
     } else if (str_req[0] == 1) {
