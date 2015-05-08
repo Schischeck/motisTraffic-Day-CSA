a7d56d86928346ebf4576da1cb215e129a8a38bd
Lars <larslotter@googlemail.com>
2015-05-09 01:29:28 +0200

    debug

--- src/execution/websocketservice.cpp
+++ src/execution/websocketservice.cpp
@@ -36,22 +36,36 @@ void websocketservice::on_message(connection_hdl hdl, websocketserver::message_p
     } else {
         //m_messages.push_back("<< " + websocketpp::utility::to_hex(msg->get_payload()));
     }
+    std::cout << "MESSAGE" << std::endl;
     std::string str_req;
     std::string str_resp;
     td::Station *tmp_station;
     str_req.append(m_messages[0]);
+    std::cout << "REQUEST: " << str_req << std::endl;
     m_messages.erase(m_messages.begin());
     if (str_req[0] == '0') {
+        std::cout << "req was 0" << std::endl;
         str_resp.clear();
+        std::cout << "str_resp_cleared" << std::endl;
+        std::cout << "size: " << this->m_stations.size() << std::endl;
         for (long unsigned int i=0; i<this->m_stations.size(); i++) {
+            /*
             tmp_station = m_stations[i].get();
+            std::cout << "tmp_station got" << std::endl;
+            if( tmp_station != NULL )
+                std::cout << "station not null" << std::endl;
             str_resp.append(std::to_string(i));
             str_resp.append(",");
             str_resp.append(std::to_string(tmp_station->length));
             str_resp.append(",");
             str_resp.append(std::to_string(tmp_station->width));
             str_resp.append(";");
+            std::cout << "appendet " << std::to_string(i) << "," << std::to_string(tmp_station->length) << std::endl;
+            */
+            //std::cout << i << std::endl;
         }
+        std::cout << "sent stations" << std::endl;
+        std::cout << str_req << std::endl;
         m_server.send(hdl, str_resp, websocketpp::frame::opcode::text);
     } else if (str_req[0] == 1) {
         //
