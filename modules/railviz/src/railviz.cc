a6aedadec4de6070c2c8538aaea7d451ebcf6648
Nikolay Semenov <semenov@ptu.tu-darmstadt.de>
2015-06-23 22:39:17 +0200

    searching for a bug

--- modules/railviz/src/railviz.cc
+++ modules/railviz/src/railviz.cc
@@ -172,12 +172,12 @@ void railviz::init()
                 p1.set<0>( station->length );
                 p1.set<1>( station->width );
 
+                motis::station* station2 = this->schedule_->stations[edge._to->get_station()->_id].get();
+                RTreePoint p2;
 //                motis::station* station2 = this->schedule_->stations[edge._to->get_station()->_id].get();
 //                RTreePoint p2;
 //                p2.set<0>( station2->length );
 //                p2.set<1>( station2->width );
-
-                RTreePoint p2;
                 p2.set<0>( station->length );
                 p2.set<1>( station->width );
 
