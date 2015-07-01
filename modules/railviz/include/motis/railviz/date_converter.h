2fed8f9c317355bb54a8b054cc9dc80cf5bbcb44
Lars <larslotter@googlemail.com>
2015-07-01 17:08:32 +0200

    railviz: all trains _maybe_ can be requested

--- modules/railviz/include/motis/railviz/date_converter.h
+++ modules/railviz/include/motis/railviz/date_converter.h
@@ -11,11 +11,13 @@ namespace railviz {
 class date_converter
 {
 public:
+    date_converter();
     date_converter( const date_manager& mgr );
+    void set_date_manager( const motis::date_manager &mgr );
     std::time_t convert(motis::time const& t) const;
     std::time_t convert(motis::date_manager::date const& d) const;
 private:
-    const date_manager& mgr;
+    const date_manager* date_manager_;
 
 };
 
--- modules/railviz/include/motis/railviz/webclient_context_manager.h
+++ modules/railviz/include/motis/railviz/webclient_context_manager.h
@@ -15,12 +15,12 @@ typedef std::unique_ptr<webclient_context> webclient_context_ptr;
 class webclient_context_manager
 {
 public:
-    webclient_context& create_webclient_context(geometry::box);
+    webclient_context& create_webclient_context();
     void remove_webclient_context(unsigned int id);
     bool webclient_context_exists(unsigned int id);
     webclient_context& get_webclient_context(unsigned int id);
 private:
-    unsigned int next_sessid;
+    static unsigned int next_sessid;
     std::map<unsigned int, webclient_context_ptr> webclient_context_map;
 };
 
