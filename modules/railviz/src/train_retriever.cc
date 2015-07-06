cff7e7e7633f06b616433bc186127f058dcedf25
Felix Gündling <felixguendling@gmail.com>
2015-07-06 21:49:59 +0200

    simplify train retriever

--- modules/railviz/src/train_retriever.cc
+++ modules/railviz/src/train_retriever.cc
@@ -22,32 +22,18 @@ std::vector<light_connection> train_retriever::connections(const time from,
                                                            int max_count,
                                                            geo::box area) {
   std::vector<light_connection> connections;
-
   for (int clasz = 0; clasz <= 9; ++clasz) {
-    auto edges = edge_index_[clasz]->edges(area);
-
-    for (auto const& e : edges) {
-      auto edge_conns = e->_m._route_edge._conns;
-      if (edge_conns[0]._full_con->clasz != clasz) {
-        continue;
-      }
-
-      for (auto const& edge_con : edge_conns) {
+    for (auto const& e : edge_index_[clasz]->edges(area)) {
+      for (auto const& edge_con : e->_m._route_edge._conns) {
         if (edge_con.a_time >= from && edge_con.d_time <= to) {
           connections.push_back(edge_con);
-
           if (connections.size() >= max_count) {
             goto end;
           }
         }
-
-        if (edge_con.d_time > to) {
-          break;
-        }
       }
     }
   }
-
 end:
   return connections;
 }
