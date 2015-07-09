c2e2f41bc5e71e92be76611226f55d131dc1c276
Felix Gündling <felixguendling@gmail.com>
2015-07-09 16:45:45 +0200

    do not link against dl when using static modules

--- motis/webservice/src/main.cc
+++ motis/webservice/src/main.cc
@@ -64,26 +64,27 @@ int main(int argc, char** argv) {
   ws_server server(ios);
   server.listen(listener_opt.host, listener_opt.port);
 
-  dispatcher dispatch(server);
+  dispatcher dispatcher(server);
 
 #ifndef MOTIS_STATIC_MODULES
-  dynamic_module_loader loader(modules_opt.path, sched.get(), dispatch, ios);
+  dynamic_module_loader loader(modules_opt.path, sched.get(), dispatcher, ios);
   loader.load_modules();
 #else
   namespace p = std::placeholders;
-  send_fun send = std::bind(&dispatch::send, &dispatch, p::_1, p::_2);
-  dispatch_fun dispatch = std::bind(&dispatch::on_msg, &dispatch, p::_1, p::_2);
+  send_fun send = std::bind(&dispatcher::send, &dispatcher, p::_1, p::_2);
+  dispatch_fun dispatch =
+      std::bind(&dispatcher::on_msg, &dispatcher, p::_1, p::_2);
 
-  std::vector<std::unique_ptr<motis::module::module>> modules;
+  std::vector<std::unique_ptr<motis::module::module> > modules;
   modules.emplace_back(new motis::guesser::guesser());
   modules.emplace_back(new motis::railviz::railviz());
   modules.emplace_back(new motis::routing::routing());
   modules.emplace_back(new motis::reliability::reliability());
 
   for (auto const& module : modules) {
-    dispatch.modules_.push_back(module.get());
+    dispatcher.modules_.push_back(module.get());
     for (auto const& subscription : module->subscriptions()) {
-      dispatch.subscriptions_.insert({subscription, module.get()});
+      dispatcher.subscriptions_.insert({subscription, module.get()});
     }
 
     module->schedule_ = sched.get();
@@ -94,7 +95,7 @@ int main(int argc, char** argv) {
 #endif
 
   std::vector<conf::configuration*> module_confs;
-  for (auto const& module : dispatch.modules_) {
+  for (auto const& module : dispatcher.modules_) {
     module_confs.push_back(module);
   }
   conf::options_parser module_conf_parser(module_confs);
