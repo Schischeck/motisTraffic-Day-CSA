fac870d757b328877e5b3482502dc97108ed06e5
Felix Gündling <felixguendling@gmail.com>
2015-07-09 11:08:32 +0200

    station guesser using the guesser library

--- modules/guesser/include/motis/guesser/guesser.h
+++ modules/guesser/include/motis/guesser/guesser.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "guess/guesser.h"
+
 #include "motis/module/module.h"
 
 namespace motis {
@@ -10,11 +12,14 @@ struct guesser : public motis::module::module {
   virtual void print(std::ostream& out) const override;
 
   virtual std::string name() const override { return "guesser"; }
+  virtual void init() override;
   virtual std::vector<MsgContent> subscriptions() const {
     return {MsgContent_StationGuesserRequest};
   }
   virtual motis::module::msg_ptr on_msg(motis::module::msg_ptr const&,
                                         motis::module::sid) override;
+
+  std::unique_ptr<guess::guesser> guesser_;
 };
 
 }  // namespace guesser
--- modules/guesser/src/guesser.cc
+++ modules/guesser/src/guesser.cc
@@ -22,21 +22,41 @@ po::options_description guesser::desc() {
 
 void guesser::print(std::ostream& out) const {}
 
+void guesser::init() {
+  std::vector<std::string> station_names(schedule_->stations.size());
+  std::transform(begin(schedule_->stations), end(schedule_->stations),
+                 begin(station_names),
+                 [](station_ptr const& s) { return s->name; });
+  guesser_ = std::unique_ptr<guess::guesser>(new guess::guesser(station_names));
+}
+
 msg_ptr guesser::on_msg(msg_ptr const& msg, sid) {
-  FlatBufferBuilder fbb;
+  auto req = msg->content<StationGuesserRequest const*>();
 
-  std::vector<Offset<String>> guesses;
-  guesses.emplace_back(fbb.CreateString("a"));
-  guesses.emplace_back(fbb.CreateString("b"));
-  guesses.emplace_back(fbb.CreateString("c"));
-  auto vec = fbb.CreateVector(std::move(guesses));
-  auto mloc = CreateStationGuesserResponse(fbb, vec);
+  FlatBufferBuilder b;
 
-  auto mmloc = motis::CreateMessage(
-      fbb, motis::MsgContent_StationGuesserResponse, mloc.Union());
-  fbb.Finish(mmloc);
+  std::vector<Offset<Station>> guesses;
+  for (auto const& guess : guesser_->guess(req->input()->str())) {
+    if (guess < 0 || guess >= schedule_->stations.size()) {
+      continue;
+    }
 
-  return make_msg(fbb);
+    auto const& station = *schedule_->stations[guess];
+    try {
+      guesses.emplace_back(
+          CreateStation(b, b.CreateString(station.name),
+                        boost::lexical_cast<int>(station.eva_nr), station.width,
+                        station.length));
+    } catch (boost::bad_lexical_cast const&) {
+      continue;
+    }
+  }
+
+  b.Finish(motis::CreateMessage(
+      b, MsgContent_StationGuesserResponse,
+      CreateStationGuesserResponse(b, b.CreateVector(guesses)).Union()));
+
+  return make_msg(b);
 }
 
 MOTIS_MODULE_DEF_MODULE(guesser)
