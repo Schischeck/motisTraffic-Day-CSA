56c080b4b00e644f8e5b1b2e173a521a5cbad47c
Felix Gündling <felixguendling@gmail.com>
2015-08-16 18:33:07 +0200

    gtfs stops parser: for loop -> std::transform

--- motis/loader/src/gtfs/stop.cc
+++ motis/loader/src/gtfs/stop.cc
@@ -1,15 +1,13 @@
 #include "motis/loader/parsers/gtfs/stop.h"
 
 #include <tuple>
-#include <array>
+#include <algorithm>
 
 #include "parser/csv.h"
 
-#include "motis/loader/parsers/gtfs/files.h"
 #include "motis/loader/util.h"
 
 using namespace flatbuffers;
-using namespace parser;
 using std::get;
 
 namespace motis {
@@ -17,19 +15,19 @@ namespace loader {
 namespace gtfs {
 
 enum { stop_id, stop_name, stop_lat, stop_lon };
-using stop = std::tuple<cstr, cstr, float, float>;
-static const column_mapping<stop> stop_columns = {
+using stop = std::tuple<parser::cstr, parser::cstr, float, float>;
+static const parser::column_mapping<stop> stop_columns = {
     {"stop_id", "stop_name", "stop_lat", "stop_lon"}};
 
 std::vector<Offset<Station>> read_stations(loaded_file file,
                                            FlatBufferBuilder& b) {
-  std::vector<Offset<Station>> stations;
-  std::vector<stop> stops = read<stop>(file.content, stop_columns);
-  for (auto const& stop : stops) {
-    stations.push_back(CreateStation(b, to_fbs_string(b, get<stop_id>(stop)),
-                                     to_fbs_string(b, get<stop_name>(stop)),
-                                     get<stop_lat>(stop), get<stop_lon>(stop)));
-  }
+  std::vector<stop> stops = parser::read<stop>(file.content, stop_columns);
+  std::vector<Offset<Station>> stations(stops.size());
+  std::transform(begin(stops), end(stops), begin(stations), [&](stop const& s) {
+    return CreateStation(b, to_fbs_string(b, get<stop_id>(s)),
+                         to_fbs_string(b, get<stop_name>(s)), get<stop_lat>(s),
+                         get<stop_lon>(s));
+  });
   return stations;
 }
 
