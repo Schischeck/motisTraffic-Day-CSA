16ca7d8123daad6c02c44f0623a93493322b053e
Tobias Raffel <raffel@algo.informatik.tu-darmstadt.de>
2015-10-12 17:09:53 +0200

    rules graph (parser) test (WIP)

--- motis/loader/include/motis/loader/parsers/hrd/service_rules/rule.h
+++ motis/loader/include/motis/loader/parsers/hrd/service_rules/rule.h
@@ -26,6 +26,7 @@ struct rule {
   virtual int applies(hrd_service const& s) const = 0;
   virtual void add(hrd_service* s, int info) = 0;
   virtual std::vector<service_combination> service_combinations() const = 0;
+  virtual resolved_rule_info rule_info() const = 0;
 
 protected:
   std::vector<std::pair<int, uint64_t>> get_ids(hrd_service const&);
