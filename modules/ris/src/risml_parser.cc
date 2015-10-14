936f5bc81841891a91fd517ba89d6454e0f2df1f
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2015-10-14 18:15:54 +0200

    reroute message

--- modules/ris/src/risml_parser.cc
+++ modules/ris/src/risml_parser.cc
@@ -50,8 +50,9 @@ xml_attribute child_attr(xml_node const& n, char const* e, char const* a) {
 }
 
 template <typename F>
-void foreach_event(xml_node const& msg, FlatBufferBuilder& fbb, F func) {
-  for (auto&& train : msg.select_nodes("./Service/ListZug/Zug")) {
+void foreach_event(FlatBufferBuilder& fbb, xml_node const& msg, F func,
+                   char const* train_selector = "./Service/ListZug/Zug") {
+  for (auto&& train : msg.select_nodes(train_selector)) {
     auto const& t_node = train.node();
     auto train_index = t_node.attribute("Nr").as_int();
 
@@ -60,10 +61,10 @@ void foreach_event(xml_node const& msg, FlatBufferBuilder& fbb, F func) {
 
       StationIdType station_id_type;
       char const* station_id;
-      auto const& eva_node = child_attr(e_node, "Bf", "EvaNr");
-      if (!eva_node.empty()) {
+      auto const& eva_attribute = child_attr(e_node, "Bf", "EvaNr");
+      if (!eva_attribute.empty()) {
         station_id_type = StationIdType_EVA;
-        station_id = eva_node.value();
+        station_id = eva_attribute.value();
       } else {
         station_id_type = StationIdType_DS100;
         station_id = child_attr(e_node, "Bf", "Code").value();
@@ -72,6 +73,7 @@ void foreach_event(xml_node const& msg, FlatBufferBuilder& fbb, F func) {
       auto event_type = parse_event_type(e_node.attribute("Typ").value());
       auto scheduled = parse_time(child_attr(e_node, "Zeit", "Soll").value());
 
+      // Event, xml_node ZE, xml_node Zug
       func(CreateEvent(fbb, station_id_type, fbb.CreateString(station_id),
                        train_index, event_type, scheduled),
            e_node, t_node);
@@ -82,7 +84,7 @@ void foreach_event(xml_node const& msg, FlatBufferBuilder& fbb, F func) {
 Offset<Message> parse_delay_msg(FlatBufferBuilder& fbb, xml_node const& msg,
                                 DelayType delay_type) {
   std::vector<Offset<UpdatedEvent>> events;
-  foreach_event(msg, fbb, [&](Offset<Event> const& event,
+  foreach_event(fbb, msg, [&](Offset<Event> const& event,
                               xml_node const& e_node, xml_node const&) {
     auto attr_name = (delay_type == DelayType_Is) ? "Ist" : "Prog";
     auto updated = parse_time(child_attr(e_node, "Zeit", attr_name).value());
@@ -95,26 +97,59 @@ Offset<Message> parse_delay_msg(FlatBufferBuilder& fbb, xml_node const& msg,
 
 Offset<Message> parse_cancel_msg(FlatBufferBuilder& fbb, xml_node const& msg) {
   std::vector<Offset<Event>> events;
-  foreach_event(msg, fbb, [&](Offset<Event> const& event, xml_node const&,
+  foreach_event(fbb, msg, [&](Offset<Event> const& event, xml_node const&,
                               xml_node const&) { events.push_back(event); });
   return CreateMessage(
       fbb, MessageUnion_CancelMessage,
       CreateCancelMessage(fbb, fbb.CreateVector(events)).Union());
 }
 
+Offset<AdditionalEvent> parse_additional_event(FlatBufferBuilder& fbb,
+                                               Offset<Event> const& event,
+                                               xml_node const& e_node,
+                                               xml_node const& t_node) {
+  auto track_attr = child_attr(e_node, "Gleis", "Soll");
+  auto track = (track_attr) ? fbb.CreateString(track_attr.value()) : 0;
+  auto category = fbb.CreateString(t_node.attribute("Gattung").value());
+  return CreateAdditionalEvent(fbb, event, category, track);
+}
+
 Offset<Message> parse_addition_msg(FlatBufferBuilder& fbb,
                                    xml_node const& msg) {
   std::vector<Offset<AdditionalEvent>> events;
-  foreach_event(msg, fbb, [&](Offset<Event> const& event,
+  foreach_event(fbb, msg, [&](Offset<Event> const& event,
                               xml_node const& e_node, xml_node const& t_node) {
-    auto track_attr = child_attr(e_node, "Gleis", "Soll");
-    auto track = (track_attr) ? fbb.CreateString(track_attr.value()) : 0;
-    auto category = fbb.CreateString(t_node.attribute("Gattung").value());
-    events.push_back(CreateAdditionalEvent(fbb, event, category, track));
+    events.push_back(parse_additional_event(fbb, event, e_node, t_node));
   });
   return CreateMessage(
       fbb, MessageUnion_AdditionMessage,
       CreateAdditionMessage(fbb, fbb.CreateVector(events)).Union());
+}
+
+Offset<Message> parse_reroute_msg(FlatBufferBuilder& fbb, xml_node const& msg) {
+  std::vector<Offset<Event>> cancelled_events;
+  foreach_event(fbb, msg,
+                [&](Offset<Event> const& event, xml_node const&,
+                    xml_node const&) { cancelled_events.push_back(event); });
+
+  std::vector<Offset<ReroutedEvent>> new_events;
+  foreach_event(
+      fbb, msg,
+      [&](Offset<Event> const& event, xml_node const& e_node,
+          xml_node const& t_node) {
+        auto additional = parse_additional_event(fbb, event, e_node, t_node);
+        cstr status_str = e_node.attribute("RegSta").value();
+        auto status = (status_str == "Normal") ? RerouteStatus_Normal
+                                               : RerouteStatus_UmlNeu;
+        new_events.push_back(CreateReroutedEvent(fbb, additional, status));
+      },
+      "./Service/ListUml/Uml/ListZug/Zug");
+
+  return CreateMessage(
+      fbb, MessageUnion_RerouteMessage,
+      CreateRerouteMessage(fbb, fbb.CreateVector(cancelled_events),
+                           fbb.CreateVector(new_events))
+          .Union());
 }
 
 Offset<Packet> parse_xml(FlatBufferBuilder& fbb, char const* xml_string) {
@@ -132,11 +167,15 @@ Offset<Packet> parse_xml(FlatBufferBuilder& fbb, char const* xml_string) {
         {{"Ist", std::bind(parse_delay_msg, _1, _2, DelayType_Is)},
          {"IstProg", std::bind(parse_delay_msg, _1, _2, DelayType_Forecast)},
          {"Ausfall", std::bind(parse_cancel_msg, _1, _2)},
-         {"Zusatzzug", std::bind(parse_addition_msg, _1, _2)}});
-    // TODO handle unknown message!?
+         {"Zusatzzug", std::bind(parse_addition_msg, _1, _2)},
+         {"Umleitung", std::bind(parse_reroute_msg, _1, _2)}});
 
     auto const& payload = msg.node().first_child();
-    messages.push_back(map[payload.name()](fbb, payload));
+    auto it = map.find(payload.name());
+    if (it != map.end()) {
+      messages.push_back(it->second(fbb, payload));
+    }
+    // TODO else: handle unknown message correctly
   }
 
   auto tout = parse_time(doc.child("Paket").attribute("TOut").value());
