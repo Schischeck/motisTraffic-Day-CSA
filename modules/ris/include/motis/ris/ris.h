1d2e666028598dce3892aac3352b22a66356995a
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2015-10-16 10:45:25 +0200

    wip...

--- modules/ris/include/motis/ris/ris.h
+++ modules/ris/include/motis/ris/ris.h
@@ -2,16 +2,33 @@
 
 #include <string>
 
+#include "boost/system/error_code.hpp"
+#include "boost/asio/deadline_timer.hpp"
+
 #include "motis/module/module.h"
+
+namespace po = boost::program_options;
 
 namespace motis {
 namespace ris {
 
 struct ris : public motis::module::module {
+  boost::program_options::options_description desc() override;
+  void print(std::ostream& out) const override;
 
-  virtual std::string name() const override { return "ris"; }
+  void init() override;
+  std::string name() override { return "ris"; }
+  std::vector<MsgContent> subscriptions() override { return {}; }
+  void on_msg(motis::module::msg_ptr msg, motis::module::sid session,
+              motis::module::callback cb) override {}
 
+private:
+  void schedule_update(boost::system::error_code e);
+
+  int update_interval_;
+  std::string zip_folder_;
+  std::unique_ptr<boost::asio::deadline_timer> timer_;
 };
 
-} // namespace ris
-} // namespace motis
+}  // namespace ris
+}  // namespace motis
--- modules/ris/src/ris.cc
+++ modules/ris/src/ris.cc
@@ -1,1 +1,50 @@
 #include "motis/ris/ris.h"
+
+#define UPDATE_INTERVAL "ris.update_interval"
+#define ZIP_FOLDER "ris.zip_folder"
+
+using boost::system::error_code;
+
+namespace motis {
+namespace ris {
+
+po::options_description ris::desc() {
+  po::options_description desc("Realtime Module");
+  // clang-format off
+  desc.add_options()
+      (UPDATE_INTERVAL,
+       po::value<int>(&update_interval_)->default_value(update_interval_),
+       "update interval in seconds")
+      (ZIP_FOLDER,
+       po::value<std::string>(&zip_folder_)->default_value(zip_folder_),
+       "folder containing RISML ZIPs");
+  // clang-format on
+  return desc;
+}
+
+void ris::print(std::ostream& out) const {
+  out << "  " << UPDATE_INTERVAL << ": " << update_interval_ << "\n"
+      << "  " << ZIP_FOLDER << ": " << zip_folder_;
+}
+
+void ris::init() {
+  timer_ = boost::asio::deadline_timer(get_thread_pool());
+  schedule_update(error_code());
+}
+
+void ris::parse_and_inject_zips() {}
+
+void ris::schedule_update(error_code e) {
+  if (e == boost::asio::error::operation_aborted) {
+    return;
+  }
+
+  parse_and_inject_zips();
+
+  timer_->expires_from_now(boost::posix_time::seconds(10));
+  timer_->async_wait(
+      std::bind(&ris::schedule_update, this, std::placeholders::_1));
+}
+
+}  // namespace ris
+}  // namespace motis
