25b35d2a843d0f8bc50c12be617b101cc357cbef
Mohammad Keyhani <keyhani@cs.tu-darmstadt.de>
2015-10-20 10:06:52 +0200

    flatbuffers-routing-response --> journey

--- motis/core/include/motis/core/common/journey_builder.h
+++ motis/core/include/motis/core/common/journey_builder.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <memory>
 #include <vector>
 
 namespace motis {
+struct category;
 struct journey;
 namespace routing {
 struct RoutingResponse;
 }
 
-std::vector<journey> to_journeys(routing::RoutingResponse const*);
+std::vector<journey> to_journeys(routing::RoutingResponse const*,
+                                 std::vector<std::unique_ptr<category>> const&);
 
 }  // namespace motis
--- motis/core/src/journey_builder.cc
+++ motis/core/src/journey_builder.cc
@@ -3,42 +3,93 @@
 #include <algorithm>
 
 #include "motis/core/common/journey.h"
+#include "motis/core/schedule/category.h"
 
 #include "motis/protocol/RoutingResponse_generated.h"
 
 namespace motis {
 
-journey::stop::event_info to_event_info(routing::EventInfo const* event,
+journey::stop::event_info to_event_info(routing::EventInfo const& event,
                                         bool const valid) {
   journey::stop::event_info e;
-  e.platform = event->platform()->c_str();
-  e.timestamp = event->time();
+  e.platform = event.platform()->c_str();
+  e.timestamp = event.time();
   e.valid = valid;
   return e;
 }
-
-journey::stop to_stop(routing::Stop const* stop, unsigned int const index,
+journey::stop to_stop(routing::Stop const& stop, unsigned int const index,
                       unsigned int const num_stops) {
   journey::stop s;
   s.index = index;
-  s.eva_no = stop->eva_nr()->c_str();
-  s.interchange = (bool)stop->interchange();
+  s.eva_no = stop.eva_nr()->c_str();
+  s.interchange = (bool)stop.interchange();
   s.lat = 0.0;
   s.lng = 0.0;
-  s.name = stop->name()->c_str();
-  s.arrival = to_event_info(stop->arrival(), index != 0);
-  s.departure = to_event_info(stop->departure(), index + 1 != num_stops);
+  s.name = stop.name()->c_str();
+  s.arrival = to_event_info(*stop.arrival(), index != 0);
+  s.departure = to_event_info(*stop.departure(), index + 1 != num_stops);
   return s;
 }
-journey::transport to_transport(routing::MoveWrapper const* move) {
+journey::transport to_transport(routing::Walk const& walk, uint16_t duration) {
   journey::transport t;
+  t.duration = duration;
+  t.from = walk.range()->from();
+  t.to = walk.range()->to();
+  t.walk = true;
+  t.category_id = 0;
+  t.category_name = "";
+  t.direction = "";
+  t.line_identifier = "";
+  t.name = "";
+  t.provider = "";
+  t.slot = 0;
+  t.train_nr = 0;
   return t;
 }
-journey::attribute to_attribute(routing::Attribute const* attribute) {
-  return journey::attribute();
+journey::transport to_transport(
+    routing::Transport const& transport, uint16_t duration,
+    std::vector<std::unique_ptr<category>> const& categories) {
+  journey::transport t;
+  t.category_name = transport.category_name()->c_str();
+  t.category_id = 0;
+  auto const it = std::find_if(categories.begin(), categories.end(),
+                               [t](std::unique_ptr<category> const& c) {
+                                 return c->name == t.category_name;
+                               });
+  if (it != categories.end()) {
+    t.category_id = it - categories.begin();
+  }
+  t.direction = transport.direction()->c_str();
+  t.duration = duration;
+  t.from = transport.range()->from();
+  t.line_identifier = transport.line_id()->c_str();
+  t.name = transport.name()->c_str();
+  t.provider = transport.provider()->c_str();
+  t.slot = 0;
+  t.to = transport.range()->to();
+  t.train_nr = transport.train_nr();
+  t.walk = false;
+  return t;
 }
 
-int get_duration(journey const& journey) {
+journey::attribute to_attribute(routing::Attribute const& attribute) {
+  journey::attribute a;
+  a.code = attribute.code()->c_str();
+  a.from = attribute.from();
+  a.text = attribute.text()->c_str();
+  a.to = attribute.to();
+  return a;
+}
+
+uint16_t get_move_duration(
+    routing::Range const& range,
+    flatbuffers::Vector<flatbuffers::Offset<routing::Stop>> const& stops) {
+  routing::Stop const& from = *stops[range.from()];
+  routing::Stop const& to = *stops[range.to()];
+  return (to.arrival()->time() - from.departure()->time()) / 60;
+}
+
+uint16_t get_duration(journey const& journey) {
   if (journey.stops.size() > 0) {
     return (journey.stops.back().arrival.timestamp -
             journey.stops.front().departure.timestamp) /
@@ -46,31 +97,48 @@ int get_duration(journey const& journey) {
   }
   return 0;
 }
-
-int get_transfers(journey const& journey) {
+uint16_t get_transfers(journey const& journey) {
   return std::count_if(journey.stops.begin(), journey.stops.end(),
                        [](journey::stop const& s) { return s.interchange; });
 }
 
-std::vector<journey> to_journeys(routing::RoutingResponse const* response) {
+std::vector<journey> to_journeys(
+    routing::RoutingResponse const* response,
+    std::vector<std::unique_ptr<category>> const& categories) {
   std::vector<journey> journeys;
   for (auto conn = response->connections()->begin();
        conn != response->connections()->end(); ++conn) {
     journeys.emplace_back();
     auto& journey = journeys.back();
+
+    /* stops */
     unsigned int stop_index = 0;
     for (auto stop = conn->stops()->begin(); stop != conn->stops()->end();
          ++stop, ++stop_index) {
       journey.stops.push_back(
-          to_stop(*stop, stop_index, conn->stops()->size()));
+          to_stop(**stop, stop_index, conn->stops()->size()));
     }
+
+    /* transports */
     for (auto transport = conn->transports()->begin();
          transport != conn->transports()->end(); ++transport) {
-      journey.transports.push_back(to_transport(*transport));
+      auto const move = *transport;
+      if (move->move_type() == routing::Move_Walk) {
+        auto walk = (routing::Walk const*)move->move();
+        journey.transports.push_back(to_transport(
+            *walk, get_move_duration(*walk->range(), *conn->stops())));
+      } else if (move->move_type() == routing::Move_Transport) {
+        auto transport = (routing::Transport const*)move->move();
+        journey.transports.push_back(to_transport(
+            *transport, get_move_duration(*transport->range(), *conn->stops()),
+            categories));
+      }
     }
+
+    /* attributes */
     for (auto attribute = conn->attributes()->begin();
          attribute != conn->attributes()->end(); ++attribute) {
-      journey.attributes.push_back(to_attribute(*attribute));
+      journey.attributes.push_back(to_attribute(**attribute));
     }
 
     journey.duration = get_duration(journey);
