e75e8e11cdca9a43e44cc9f239697603155290a0
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2015-11-06 16:57:56 +0100

    osrm module

---
+++ modules/osrm/include/motis/osrm/osrm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "motis/osrm/osrm.h"
+
+#include "motis/module/module.h"
+
+namespace motis {
+namespace osrm {
+
+struct osrm : public motis::module::module {
+  virtual ~osrm() {}
+
+  virtual boost::program_options::options_description desc() override;
+  virtual void print(std::ostream& out) const override;
+  virtual bool empty_config() const override { return true; }
+
+  virtual std::string name() const override { return "osrm"; }
+  virtual void init() override;
+  virtual std::vector<MsgContent> subscriptions() const override { return {}; }
+  virtual void on_msg(motis::module::msg_ptr, motis::module::sid,
+                      motis::module::callback) override;
+};
+
+}  // namespace osrm
+}  // namespace motis
