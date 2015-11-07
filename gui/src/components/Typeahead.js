fca03cfc1fec4de97c8bf38841068fabcb3b28a0
Felix Gündling <felixguendling@gmail.com>
2015-11-08 00:50:58 +0100

    es formatter

--- gui/src/components/Typeahead.js
+++ gui/src/components/Typeahead.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import {Container} from 'flux/utils';
+import { Container } from 'flux/utils';
 
-import {TextField, List, ListItem, Paper} from 'material-ui/lib';
+import { TextField, List, ListItem, Paper } from 'material-ui/lib';
 
 import './Typeahead.scss';
 
@@ -26,29 +26,50 @@ export class Typeahead extends React.Component {
 
   _fetchCompletions(evt) {
     this.props.complete(evt.target.value).then(response => {
-      this.setState({ completions: response });
+      this.setState({
+        completions: response
+      });
     });
   }
 
   _onChange(e) {
-    this.setState({ value: e.target.value });
+    this.setState({
+      value: e.target.value
+    });
     this._fetchCompletions(e);
   }
 
-  _selectGuess(value) { this.setState({ value: value, completions: [] }); }
+  _selectGuess(value) {
+    this.setState({
+      value: value,
+      completions: []
+    });
+  }
 
-  _clearCompletions() { setTimeout(e => { this.setState({ completions: [] }); }, 280); }
+  _clearCompletions() {
+    setTimeout(e => {
+      this.setState({
+        completions: []
+      });
+    }, 280);
+  }
+
+  _updateSelectedIndex(index) {
+    this.setState({
+      selectedItemIndex: mod(index, this.state.completions.length)
+    });
+  }
 
   _onKeyUp(e) {
-    switch(e.keyCode) {
+    switch (e.keyCode) {
       // UP
       case 38:
-        this.setState({ selectedItemIndex: mod(this.state.selectedItemIndex - 1, this.state.completions.length) });
+        this._updateSelectedIndex(this.state.selectedItemIndex - 1);
         break;
 
       // DOWN
       case 40:
-        this.setState({ selectedItemIndex: mod(this.state.selectedItemIndex + 1, this.state.completions.length) });
+        this._updateSelectedIndex(this.state.selectedItemIndex + 1);
         break;
 
       // RETURN
@@ -56,7 +77,7 @@ export class Typeahead extends React.Component {
       // ENTER
       case 14:
         this.setState({
-          selectedItemIndex: -1,
+          selectedItemIndex: 0,
           value: this.state.completions[this.state.selectedItemIndex].name,
           completions: []
         });
@@ -71,34 +92,37 @@ export class Typeahead extends React.Component {
 
   render() {
     return (
-      <div style={{'width': 256, 'position': 'relative'}}>
-        <TextField
-          hintText={ this.props.name }
-          value={ this.state.value }
-          onChange={ this._onChange.bind(this) }
-          onKeyUp={ this._onKeyUp.bind(this) }
-          onBlur={ this._clearCompletions.bind(this) }>
-        </TextField>
-        <Paper ref={'suggestionbox'} zDepth={1} className={['suggestions']}>
-          <List desktop={true}>
-            {
-              this.state.completions.map((val, index) => {
-                let style = {};
-                if (this.state.selectedItemIndex === index) {
-                  style = { backgroundColor: 'rgba(0, 0, 0, 0.0980392)' };
-                }
-                return <ListItem
-                          style={ style }
-                          hovered={ true }
-                          key={ index }
-                          primaryText={ val.name }
-                          onMouseEnter={ () => { this.setState({ selectedItemIndex: index }); } }
-                          onClick={ this._selectGuess.bind(this, val.name) } />
-              })
-            }
-          </List>
-        </Paper>
-      </div>
+    <div style={ {  'width': 256,  'position': 'relative'} }>
+      <TextField
+                 hintText={ this.props.name }
+                 value={ this.state.value }
+                 onChange={ this._onChange.bind(this) }
+                 onKeyUp={ this._onKeyUp.bind(this) }
+                 onBlur={ this._clearCompletions.bind(this) }>
+      </TextField>
+      <Paper
+             ref={ 'suggestionbox' }
+             zDepth={ 1 }
+             className={ [  'suggestions'] }>
+        <List desktop={ true }>
+          { this.state.completions.map((val, index) => {
+              let style = {};
+              if (this.state.selectedItemIndex === index) {
+                style = {
+                  backgroundColor: 'rgba(0, 0, 0, 0.0980392)'
+                };
+              }
+              return <ListItem
+                               style={ style }
+                               hovered={ true }
+                               key={ index }
+                               primaryText={ val.name }
+                               onMouseEnter={ this._updateSelectedIndex.bind(this, index) }
+                               onClick={ this._selectGuess.bind(this, val.name) } />
+            }) }
+        </List>
+      </Paper>
+    </div>
     );
   }
 }
