d3ff789df2bc5f011d8f307f7e2e143c84443c11
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2015-11-12 10:23:33 +0100

    crash destroy!!!

--- test/test_main.cc
+++ test/test_main.cc
@@ -8,9 +8,17 @@
 
 namespace fs = boost::filesystem;
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   fs::current_path(MOTIS_TEST_EXECUTION_DIR);
 
   ::testing::InitGoogleTest(&argc, argv);
+
+  bool var;
+  if (var) {
+    printf("listening for requests");
+  }
+
+  std::cout << "crash!\n" << *(static_cast<char*>(nullptr)) << "\n";
+
   return RUN_ALL_TESTS();
 }
