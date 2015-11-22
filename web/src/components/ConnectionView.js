f67314d64d0abf223251acd64fc17cb88916b999
Felix Gündling <felixguendling@gmail.com>
2015-11-22 17:45:16 +0100

    gui: train names display

--- web/src/components/ConnectionView.js
+++ web/src/components/ConnectionView.js
@@ -33,11 +33,23 @@ export default class ConnectionView extends Component {
     return <span className={ style.trainId }>{ con.transports[interchange].move.name }</span>;
   }
 
+  makeTrainNames(con, from, to, key) {
+    return (<ul key={ key } className={style.trainNames}>
+              { con.transports.filter(t => {
+                  return t.move.range.from >= from && t.move.range.to <= to;
+                }).map(t => {
+                  return <li>
+                           { t.move.name }
+                         </li>;
+                }) }
+            </ul>);
+  }
+
   render() {
     if (this.props.waiting) {
       return (<CircularProgress
                                 mode="indeterminate"
-                                style={ { 'margin': '10px auto', 'display': 'block'} } />);
+                                style={ {  'margin': '10px auto',  'display': 'block'} } />);
     }
 
     if (this.props.showError) {
@@ -56,23 +68,40 @@ export default class ConnectionView extends Component {
     return (
     <div className={ style.conWrap }>
       <ul className={ style.con }>
-        { con.stops.filter((stop, i) => {
-            return i === 1 || i === con.stops.length - 2 || stop.interchange;
-          }).map((stop, i, src) => {
-            console.log(stop);
-            const attrs = this.makeAttributes(con, i + 1);
-            const arr = i !== 0 ? this.makeArrivalElement(stop.arrival.time) : <div />;
-            const dep = i !== src.length - 1 ? this.makeArrivalElement(stop.departure.time) : <div />;
-            return ( <li
-                         key={ i }
-                         className={ style.stop }>
-                       { arr }
-                       <div className={ style.stationname }>
-                         { stop.name }
-                         { attrs }
-                       </div>
-                       { dep }
-                     </li> );
+        { con.stops.map((stop, i) => {
+            return {
+              type: 'stop',
+              stop,
+              i
+            };
+          }).filter((el, i) => {
+            return i === 1 || i === con.stops.length - 2 || el.stop.interchange;
+          }).reduce((acc, curr) => {
+            if (acc.length !== 0) {
+              acc.push({
+                type: 'transport',
+                from: acc[acc.length - 1].i,
+                to: curr.i
+              });
+            }
+            acc.push(curr);
+            return acc;
+          }, []).map((el, index) => {
+            if (el.type == 'stop') {
+              const arr = el.i !== 1 ? this.makeArrivalElement(el.stop.arrival.time) : <div />;
+              const dep = el.i !== con.stops.length - 2 ? this.makeArrivalElement(el.stop.departure.time) : <div />;
+              return ( <li
+                           key={ index }
+                           className={ style.stop }>
+                         { arr }
+                         <div className={ style.stationName }>
+                           { el.stop.name }
+                         </div>
+                         { dep }
+                       </li> );
+            } else {
+              return this.makeTrainNames(con, el.from, el.to, index);
+            }
           }) }
       </ul>
     </div>
