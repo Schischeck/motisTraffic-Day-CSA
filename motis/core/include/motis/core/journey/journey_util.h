65505eebed9b5a03dc44ddd05ceb1fd812511761
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2015-11-24 12:12:59 +0100

    journey accessor

--- motis/core/include/motis/core/journey/journey_util.h
+++ motis/core/include/motis/core/journey/journey_util.h
@@ -2,10 +2,24 @@
 
 #include <cstdint>
 
+#include "motis/core/journey/journey.h"
+
 namespace motis {
-struct journey;
 
 uint16_t get_duration(journey const&);
 uint16_t get_transfers(journey const&);
+
+template <typename F>
+void foreach_light_connection(journey const& journey, F function) {
+  for (auto const& transport : journey.transports) {
+    if (transport.walk) {
+      continue;
+    }
+
+    for (auto i = transport.from; i < transport.to; ++i) {
+      function(transport, journey.stops[i], journey.stops[i + 1]);
+    }
+  }
+}
 
 }  // namespace motis
