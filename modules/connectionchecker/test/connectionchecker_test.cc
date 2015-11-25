1fbca9e7d1b89379049251d747b927004ece7528
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2015-11-25 10:37:07 +0100

    connectionchecker: test schedule

---
+++ modules/connectionchecker/test/connectionchecker_test.cc
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+namespace motis {
+namespace connectionchecker {
+
+TEST(connectionchecker, finds_annotated_connections) {
+  ASSERT_TRUE(true);
+}
+
+} // namespace connectionchecker
+} // namespace motis
