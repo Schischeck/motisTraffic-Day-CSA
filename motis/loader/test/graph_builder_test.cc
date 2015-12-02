7a27576d40556c3f622b5949ac710cc146798e3c
Tobias Raffel <raffel@algo.informatik.tu-darmstadt.de>
2015-12-02 11:16:01 +0100

    new testcase graph builder: season service valid & invalid

--- motis/loader/test/graph_builder_test.cc
+++ motis/loader/test/graph_builder_test.cc
@@ -89,14 +89,6 @@ public:
                                   to_unix_time(2015, 9, 12)) {}
 };
 
-class loader_graph_builder_east_to_west_test
-    : public loader_graph_builder_test {
-public:
-  loader_graph_builder_east_to_west_test()
-      : loader_graph_builder_test("east-to-west", to_unix_time(2015, 7, 2),
-                                  to_unix_time(2015, 7, 10)) {}
-};
-
 TEST_F(loader_multiple_ice_multiple_ice_graph_builder_test, eva_num) {
   auto& stations = sched_->eva_to_station;
   EXPECT_STREQ("8000013", stations["8000013"]->eva_nr.c_str());
@@ -400,6 +392,14 @@ time exp_time(int day_idx, int hhmm, int offset) {
   return (day_idx + 1 * MINUTES_A_DAY) + hhmm_to_min(hhmm) - offset;
 }
 
+class loader_graph_builder_east_to_west_test
+    : public loader_graph_builder_test {
+public:
+  loader_graph_builder_east_to_west_test()
+      : loader_graph_builder_test("east-to-west", to_unix_time(2015, 7, 2),
+                                  to_unix_time(2015, 7, 10)) {}
+};
+
 TEST_F(loader_graph_builder_east_to_west_test, event_times) {
   // Get route starting at Moskva Belorusskaja
   auto node_it = std::find_if(
@@ -413,6 +413,56 @@ TEST_F(loader_graph_builder_east_to_west_test, event_times) {
   // GMT+3 -> GMT+1 (season time)
   test_events(cs.at(8), exp_time(0, 3106, 180), exp_time(0, 3018, 120));
   test_events(cs.at(22), exp_time(0, 5306, 120), exp_time(0, 5716, 120));
+}
+
+class loader_graph_builder_season_valid : public loader_graph_builder_test {
+public:
+  loader_graph_builder_season_valid()
+      : loader_graph_builder_test("season-valid", to_unix_time(2015, 3, 29),
+                                  to_unix_time(2015, 3, 31)) {}
+};
+
+TEST_F(loader_graph_builder_season_valid, event_times) {
+  // Get route starting at Dortmund Hbf
+  auto node_it = std::find_if(
+      begin(sched_->route_index_to_first_route_node),
+      end(sched_->route_index_to_first_route_node), [&](node const* n) {
+        return sched_->stations[n->get_station()->_id]->eva_nr == "8000080";
+      });
+  ASSERT_TRUE(node_it != end(sched_->route_index_to_first_route_node));
+
+  auto cs = get_connections(*node_it, 0);
+  ASSERT_EQ(38, cs.size());
+
+  test_events(cs.at(0), exp_time(0, 53, 60), exp_time(0, 55, 60));
+  // +1 -> +2
+  test_events(cs.at(20), exp_time(0, 158, 60), exp_time(0, 300, 120));
+  test_events(cs.at(37), exp_time(0, 344, 120), exp_time(0, 347, 120));
+}
+
+// TODO implement solution for invalid times
+class loader_graph_builder_season_invalid : public loader_graph_builder_test {
+public:
+  loader_graph_builder_season_invalid()
+      : loader_graph_builder_test("season-invalid", to_unix_time(2015, 3, 29),
+                                  to_unix_time(2015, 3, 31)) {}
+};
+
+TEST_F(loader_graph_builder_season_invalid, event_times) {
+  // Get route starting at Muenster(Westf)Hbf
+  auto node_it = std::find_if(
+      begin(sched_->route_index_to_first_route_node),
+      end(sched_->route_index_to_first_route_node), [&](node const* n) {
+        return sched_->stations[n->get_station()->_id]->eva_nr == "8000263";
+      });
+  ASSERT_TRUE(node_it != end(sched_->route_index_to_first_route_node));
+
+  auto cs = get_connections(*node_it, 0);
+  ASSERT_EQ(10, cs.size());
+
+  test_events(cs.at(0), exp_time(0, 108, 60), exp_time(0, 111, 60));
+  // +1 -> +2
+  test_events(cs.at(9), exp_time(0, 154, 60), exp_time(0, 204, 120));
 }
 
 }  // loader
