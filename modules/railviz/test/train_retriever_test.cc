4e2f90e1433e665338c3c0f3ca203162291e8895
Jonas Schlitzer <jonas_werner.schlitzer@stud.tu-darmstadt.de>
2015-12-02 16:32:48 +0100

    fixed memory leak

--- modules/railviz/test/train_retriever_test.cc
+++ modules/railviz/test/train_retriever_test.cc
@@ -14,10 +14,10 @@ TEST(railviz_all_train, simple_test){
                                         "02_test_set/",
                                         true, false, to_unix_time(2015, 11, 21),
                                         to_unix_time(2015, 11, 22));																	
-  auto train_retriever_ = new train_retriever(*schedule);
+  train_retriever ttr(*schedule);
 	geo::box area = {{49.8728, 8.24411},
                    {50.969, 9.79821}};
-	std::vector<std::pair<light_connection const*, edge const*>> trains = train_retriever_->trains(2800, 3100, 1000, area);
+	std::vector<std::pair<light_connection const*, edge const*>> trains = ttr.trains(2800, 3100, 1000, area);
 	ASSERT_NE(0, trains.size());
 	ASSERT_EQ(5, trains.size());
 }
