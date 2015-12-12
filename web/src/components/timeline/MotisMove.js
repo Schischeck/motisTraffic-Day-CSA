c992d16ff907bd3563b299c43af39e4c8f80afe5
Felix Gündling <felixguendling@gmail.com>
2015-12-12 23:16:02 +0100

    wip

--- web/src/components/timeline/MotisMove.js
+++ web/src/components/timeline/MotisMove.js
@@ -8,32 +8,59 @@ SVG.MotisMove = SVG.invent({
       return this.offset;
     },
 
-    draw: function(thickness, radius, desired_len, label) {
-      var in_eq_width = Math.cos(Math.asin((thickness / 2.0) / radius)) * radius;
-      var x_offset = in_eq_width + radius;
-      var y_offset = -thickness / 2.0 + radius;
-      var x = x_offset;
-      var y = y_offset;
-      var len = desired_len;
-      len -= radius + in_eq_width;
-      len -= thickness / 6.0;
+    setDrawParameters: function(thickness, radius, desired_len) {
+      this.thickness = thickness;
+      this.radius = radius;
 
-      var minWidth = in_eq_width / 2.0;
-      this.offset = minWidth - len;
+      this.in_eq_width = Math.cos(Math.asin((thickness / 2.0) / radius)) * radius;
+      this.x_offset = this.in_eq_width + radius;
+      this.y_offset = -thickness / 2.0 + radius;
+      this.x = this.x_offset;
+      this.y = this.y_offset;
+
+      this.setDesiredLength(desired_len);
+    },
+
+    setDesiredLength: function(desired_len) {
+      var minWidth = this.in_eq_width / 2.0;
+      var len = desired_len - (this.radius + this.in_eq_width) - (this.thickness / 6.0);
+      var offset = minWidth - len;
       if (len < minWidth) {
         len = minWidth;
       }
 
+      this.len = len;
+      this.offset = offset;
+    },
+
+    generatePathData: function() {
+      return 'm' + this.in_eq_width + ',' + 0 +
+             'a' + this.radius + ',' + this.radius + ' 0 1,0 0,' + this.thickness +
+             'h ' + this.len +
+             'v -' + this.thickness +
+             'z' +
+             'M' + (this.x + this.len - this.radius) + ',' + (this.y - this.y_offset) +
+             'a' + this.radius + ',' + this.radius + ' 0 0,0 0,' + this.thickness;
+    },
+
+    updatePath: function() {
+      this.path.plot(this.generatePathData());
+    },
+
+    updateLength: function(desired_len) {
+      this.setDesiredLength(desired_len);
+      this.updatePath();
+    },
+
+    draw: function(thickness, radius, desired_len, label) {
+      this.setDrawParameters(thickness, radius, desired_len);
+
       var g = this.put(new SVG.G);
-      g.add(this.put(new SVG.Path)
-                .attr({'fill-rule': 'evenodd'})
-                .plot('m' + in_eq_width + ',' + 0 +
-                      'a' + radius + ',' + radius + ' 0 1,0 0,' + thickness +
-                      'h ' + len +
-                      'v -' + thickness +
-                      'z' +
-                      'M' + (x + len - radius) + ',' + (y - y_offset) +
-                      'a' + radius + ',' + radius + ' 0 0,0 0,' + thickness));
+      this.path = this.put(new SVG.Path)
+                      .attr({'fill-rule': 'evenodd'});
+      this.updatePath();
+
+      g.add(this.path);
       g.add(this.put(new SVG.Text())
                 .text(label || '???')
                 .attr({'fill': '#FFF'})
@@ -46,6 +73,7 @@ SVG.MotisMove = SVG.invent({
       return g;
     }
   },
+
   construct: {
     motismove: function(thickness, radius, len) {
       return this
