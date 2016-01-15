82593571cf9acb378a52ebd51cab8fd82d2a9d12
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2016-01-15 18:07:17 +0100

    geoindex api first prototype

--- modules/intermodal/include/motis/intermodal/intermodal.h
+++ modules/intermodal/include/motis/intermodal/intermodal.h
@@ -18,10 +18,16 @@ struct intermodal : public motis::module::module {
   virtual std::string name() const override { return "intermodal"; }
   virtual void init() override;
   virtual std::vector<MsgContent> subscriptions() const override {
-    return {MsgContent_IntermodalRequest};
+    return {MsgContent_IntermodalRoutingRequest,
+            MsgContent_IntermodalGeoIndexRequest};
   }
   virtual void on_msg(motis::module::msg_ptr, motis::module::sid,
                       motis::module::callback) override;
+
+  void handle_routing_request(IntermodalRoutingRequest const*,
+                              motis::module::callback);
+  void handle_geo_index_request(IntermodalGeoIndexRequest const*,
+                                motis::module::callback);
 
   struct impl;
   std::unique_ptr<impl> impl_;
