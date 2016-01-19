19ea05004c6400c235827fe85825f12dbc06fb37
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-01-19 22:15:31 +0100

    introduce integration tests: *_itest.cc

---
+++ base/loader/test/graph_builder/rule_service_itest.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+
+namespace motis {
+namespace loader {
+
+TEST(loader_graph_builder_rule_service, search) {
+  // TODO
+  EXPECT_TRUE(true);
+}
+
+}  // namespace loader
+}  // namespace motis
