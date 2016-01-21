9370fa60d850fb98a074ad6fb0d0166a466388d0
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-01-21 20:14:24 +0100

    array datastructure: fix rend iterator
    (cherry picked from commit d0844491e7dd22a9cdc8d366b37457af00b84f38)

--- base/core/include/motis/core/common/array.h
+++ base/core/include/motis/core/common/array.h
@@ -122,14 +122,12 @@ struct array final {
     return std::reverse_iterator<T*>(_el + size());
   }
   std::reverse_iterator<T const*> rend() const {
-    return std::reverse_iterator<T*>(_el - 1);
+    return std::reverse_iterator<T*>(_el);
   }
   std::reverse_iterator<T*> rbegin() {
     return std::reverse_iterator<T*>(_el + size());
   }
-  std::reverse_iterator<T*> rend() {
-    return std::reverse_iterator<T*>(_el - 1);
-  }
+  std::reverse_iterator<T*> rend() { return std::reverse_iterator<T*>(_el); }
 
   friend T const* begin(array const& a) { return a.begin(); }
   friend T const* end(array const& a) { return a.end(); }
@@ -183,9 +181,6 @@ struct array final {
   void resize(size_type size) {
     reserve(size);
     _used_size = size;
-    for (size_type i = 0; i < size; ++i) {
-      new (_el + i) T();
-    }
   }
 
   void reserve(TemplateSizeType new_size) {
