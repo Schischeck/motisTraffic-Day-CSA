c946198f53a4227075e6bf31b3ff359bffcbb6fa
Felix Gündling <felixguendling@gmail.com>
2016-01-25 20:05:17 +0100

    routing: different tie breakers (during search and after search)

--- modules/routing/include/motis/routing/label/dominance.h
+++ modules/routing/include/motis/routing/label/dominance.h
@@ -3,24 +3,25 @@
 namespace motis {
 namespace routing {
 
-template <typename... Dominators>
+template <typename TieBreaker, typename... Dominators>
 struct dominance;
 
-template <typename FirstDominator, typename... RestDominators>
-struct dominance<FirstDominator, RestDominators...> {
+template <typename TieBreaker, typename FirstDominator,
+          typename... RestDominators>
+struct dominance<TieBreaker, FirstDominator, RestDominators...> {
   template <typename Label>
   static bool dominates(bool could_dominate, Label const& a, Label const& b) {
     auto d = FirstDominator::dominates(a, b);
-    return !d.greater() && dominance<RestDominators...>::dominates(
+    return !d.greater() && dominance<TieBreaker, RestDominators...>::dominates(
                                could_dominate || d.smaller(), a, b);
   }
 };
 
-template <>
-struct dominance<> {
+template <typename TieBreaker>
+struct dominance<TieBreaker> {
   template <typename Label>
-  static bool dominates(bool, Label const&, Label const&) {
-    return true;
+  static bool dominates(bool could_dominate, Label const& a, Label const& b) {
+    return TieBreaker::dominates(could_dominate, a, b);
   }
 };
 
--- modules/routing/include/motis/routing/search.h
+++ modules/routing/include/motis/routing/search.h
@@ -17,6 +17,7 @@
 #include "motis/routing/label/filter.h"
 #include "motis/routing/label/comparator.h"
 #include "motis/routing/label/dominance.h"
+#include "motis/routing/label/tie_breakers.h"
 #include "motis/routing/label/criteria/transfers.h"
 #include "motis/routing/label/criteria/travel_time.h"
 
@@ -34,13 +35,14 @@ struct search_result {
   std::vector<journey> journeys;
 };
 
-typedef label<label_data<travel_time, transfers>,
-              initializer<travel_time_initializer, transfers_initializer>,
-              updater<travel_time_updater, transfers_updater>,
-              filter<travel_time_filter, transfers_filter, waiting_time_filter>,
-              dominance<travel_time_dominance, transfers_dominance>,
-              dominance<travel_time_alpha_dominance, transfers_dominance>,
-              comparator<travel_time_dominance, transfers_dominance>> my_label;
+typedef label<
+    label_data<travel_time, transfers>,
+    initializer<travel_time_initializer, transfers_initializer>,
+    updater<travel_time_updater, transfers_updater>,
+    filter<travel_time_filter, transfers_filter, waiting_time_filter>,
+    dominance<default_tb, travel_time_dominance, transfers_dominance>,
+    dominance<post_search_tb, travel_time_alpha_dominance, transfers_dominance>,
+    comparator<travel_time_dominance, transfers_dominance>> my_label;
 
 class search {
 public:
