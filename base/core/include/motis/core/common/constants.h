051b72e16afab0eee1e08c6cf85a219093ff3aac
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2016-01-28 10:35:44 +0100

    more constants

--- base/core/include/motis/core/common/constants.h
+++ base/core/include/motis/core/common/constants.h
@@ -3,5 +3,12 @@
 namespace motis {
 
 constexpr int WALK_SPEED = 1;  // m/s
+constexpr int BIKE_SPEED = 15;  // m/s
+
+constexpr int MAX_WALK_TIME = 10 * 60; // s
+constexpr int MAX_BIKE_TIME = 30 * 60; // s
+
+constexpr int MAX_WALK_DIST = MAX_WALK_TIME * WALK_SPEED; // m
+constexpr int MAX_BIKE_DIST = MAX_BIKE_TIME * BIKE_SPEED; // m
 
 }  // namespace motis
