74e24db55d3f7b6f8549a4ae5385da59ed68a5c0
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2016-02-04 18:29:17 +0100

    auto constants

--- base/core/include/motis/core/common/constants.h
+++ base/core/include/motis/core/common/constants.h
@@ -2,17 +2,17 @@
 
 namespace motis {
 
-constexpr double WALK_SPEED = 1.5;  // m/s
-constexpr int BIKE_SPEED = 15;  // m/s
+constexpr auto WALK_SPEED = 1.5;  // m/s
+constexpr auto BIKE_SPEED = 15;  // m/s
 
-constexpr int MAX_WALK_TIME = 10 * 60; // s
-constexpr int MAX_BIKE_TIME = 30 * 60; // s
+constexpr auto MAX_WALK_TIME = 10 * 60; // s
+constexpr auto MAX_BIKE_TIME = 30 * 60; // s
 
-constexpr int MAX_WALK_DIST = MAX_WALK_TIME * WALK_SPEED; // m
-constexpr int MAX_BIKE_DIST = MAX_BIKE_TIME * BIKE_SPEED; // m
+constexpr auto MAX_WALK_DIST = MAX_WALK_TIME * WALK_SPEED; // m
+constexpr auto MAX_BIKE_DIST = MAX_BIKE_TIME * BIKE_SPEED; // m
 
-constexpr int MAX_TRAVEL_TIME_HOURS = 24;
-constexpr int MAX_TRAVEL_TIME_MINUTES = MAX_TRAVEL_TIME_HOURS * 60;
-constexpr int MAX_TRAVEL_TIME_SECONDS = MAX_TRAVEL_TIME_MINUTES * 60;
+constexpr auto MAX_TRAVEL_TIME_HOURS = 24;
+constexpr auto MAX_TRAVEL_TIME_MINUTES = MAX_TRAVEL_TIME_HOURS * 60;
+constexpr auto MAX_TRAVEL_TIME_SECONDS = MAX_TRAVEL_TIME_MINUTES * 60;
 
 }  // namespace motis
