e4de52edd8d599a5b4e1a021ec1d1b20940cee32
felix <felix.guendling@gmail.com>
2016-04-19 23:05:01 +0200

    win32 compatibility (todo: formatting)

--- base/module/include/motis/module/registry.h
+++ base/module/include/motis/module/registry.h
@@ -12,56 +12,17 @@ namespace module {
 
 using op = std::function<msg_ptr(msg_ptr const&)>;
 
-namespace detail {
-// void fn(void)
-template <typename Fn>
-inline auto wrap_op(Fn fn) ->
-    typename std::enable_if<std::is_same<decltype(fn()), void>::value,
-                            op>::type {
-  return [fn = std::move(fn)](msg_ptr const&) {
-    fn();
-    return make_success_msg();
-  };
-}
-
-// msg_ptr fn(void)
-template <typename Fn>
-inline auto wrap_op(Fn fn) ->
-    typename std::enable_if<std::is_same<decltype(fn()), msg_ptr>::value,
-                            op>::type {
-  return [fn = std::move(fn)](msg_ptr const&) { return fn(); };
-}
-
-// void fn(msg_ptr)
-template <typename Fn>
-inline auto wrap_op(Fn fn) -> typename std::enable_if<
-    std::is_same<decltype(fn(make_success_msg())), void>::value, op>::type {
-  return [fn = std::move(fn)](msg_ptr const& msg) {
-    fn(msg);
-    return make_success_msg();
-  };
-}
-
-// msg_ptr fn(msg_ptr)
-template <typename Fn>
-inline auto wrap_op(Fn fn) -> typename std::enable_if<
-    std::is_same<decltype(fn(make_success_msg())), msg_ptr>::value, op>::type {
-  return [fn = std::move(fn)](msg_ptr const& msg) { return fn(msg); };
-}
-
-}  // namespace detail
-
 struct registry {
   template <typename Fn>
   void register_op(std::string const& name, Fn fn) {
-    if (!operations_.emplace(name, detail::wrap_op(std::move(fn))).second) {
+    if (!operations_.emplace(name, std::forward<Fn>(fn)).second) {
       throw std::runtime_error("target already registered");
     }
   }
 
   template <typename Fn>
   void subscribe(std::string const& topic, Fn fn) {
-    topic_subscriptions_[topic].emplace_back(detail::wrap_op(std::move(fn)));
+    topic_subscriptions_[topic].emplace_back(std::forward<Fn>(fn));
   }
 
   schedule* sched_;
