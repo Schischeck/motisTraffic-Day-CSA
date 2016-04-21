561489a7d0241c8a0dc57f6e7fc09791d7932f0d
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-04-21 17:09:21 +0200

    fix trip access

--- base/core/include/motis/core/access/trip_access.h
+++ base/core/include/motis/core/access/trip_access.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 
@@ -16,29 +17,28 @@ inline trip const* get_trip(schedule const& sched, std::string const& eva_nr,
                             std::string const& target_eva_nr,
                             std::time_t const target_timestamp,
                             bool const is_arrival, std::string const& line_id) {
-  auto station_id = get_station(sched, eva_nr)->index_;
-  auto motis_time = unix_to_motistime(sched, timestamp);
+  auto const station_id = get_station(sched, eva_nr)->index_;
+  auto const motis_time = unix_to_motistime(sched, timestamp);
+  auto const primary_id = primary_trip_id(station_id, train_nr, motis_time);
 
-  auto map_it = sched.trips_.find({station_id, train_nr, motis_time});
-  if (map_it == end(sched.trips_)) {
+  auto it =
+      std::lower_bound(begin(sched.trips_), end(sched.trips_),
+                       std::make_pair(primary_id, static_cast<trip*>(nullptr)));
+  if (it == end(sched.trips_) || !(it->first == primary_id)) {
     throw std::system_error(access::error::service_not_found);
   }
 
-  auto target_station_id = get_station(sched, target_eva_nr)->index_;
-  auto target_motis_time = unix_to_motistime(sched, target_timestamp);
-
-  auto vec = map_it->second;
-  auto vec_it = std::find_if(begin(vec), end(vec), [&](trip const* t) {
-    auto const& s = t->id_.secondary_;
-    return line_id == s.line_id_ && target_station_id == s.target_station_id_ &&
-           target_motis_time == s.target_time_ && is_arrival == s.is_arrival_;
-  });
-
-  if (vec_it == end(vec)) {
-    throw std::system_error(access::error::service_not_found);
+  auto const target_station_id = get_station(sched, target_eva_nr)->index_;
+  auto const target_motis_time = unix_to_motistime(sched, target_timestamp);
+  for (; it->first == primary_id && it != end(sched.trips_); ++it) {
+    auto const& s = it->second->id_.secondary_;
+    if (line_id == s.line_id_ && target_station_id == s.target_station_id_ &&
+        target_motis_time == s.target_time_ && is_arrival == s.is_arrival_) {
+      return it->second;
+    }
   }
 
-  return *vec_it;
+  throw std::system_error(access::error::service_not_found);
 }
 
 }  // namespace motis
