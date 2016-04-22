936d39904fae58dd0ec7d54352f40c2fff5f0a20
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2016-04-22 10:42:27 +0200

    user module stub

---
+++ modules/user/src/user.cc
@@ -0,0 +1,53 @@
+#include "motis/user/user.h"
+
+#include "pgdb/pgdb.h"
+
+#include "motis/module/message.h"
+
+using namespace motis::module;
+using namespace pgdb;
+
+namespace motis {
+namespace user {
+
+constexpr char kCreateUser[] = R"sql(
+CREATE TABLE IF NOT EXISTS users (
+  id BIGSERIAL PRIMARY KEY,
+  name text
+);
+)sql";
+using create_users_table = void_stmt<kCreateUser>;
+
+constexpr char kInsertUser[] =
+    "INSERT INTO users ( name ) VALUES ( $1 ) RETURNING id;";
+using insert_user =
+    prep_stmt<kInsertUser, std::tuple<std::string>, std::tuple<long>>;
+
+user::user() : module("User", "user") {
+  string_param(conninfo_, "dbname=postgres", "conninfo",
+               "How to connect to a postgres database.");
+}
+
+void user::init(registry& r) {
+  connection_handle conn(conninfo_);
+  create_users_table::exec(conn);
+
+  r.register_op("/user/register",
+                [this](msg_ptr const& m) { return register_user(m); });
+}
+
+msg_ptr user::register_user(msg_ptr const& msg) {
+  auto req = motis_content(UserRegisterRequest, msg);
+
+  auto username = req->name()->str();
+
+  connection_handle conn(conninfo_);
+  auto id = single_val<insert_user>(conn, username);
+
+  std::cout << "registered user " << username << " with id " << id << "\n";
+
+  return make_success_msg();
+}
+
+}  // namespace user
+}  // namespace motis
