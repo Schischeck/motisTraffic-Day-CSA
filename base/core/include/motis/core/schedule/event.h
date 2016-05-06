944dec4279758d1cd4cf7d5942be6ffd63ef53bb
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-05-06 17:17:23 +0200

    WIP: rt

--- base/core/include/motis/core/schedule/event.h
+++ base/core/include/motis/core/schedule/event.h
@@ -10,138 +10,68 @@
 namespace motis {
 
 struct base_event {
-  base_event(unsigned station_index, uint32_t train_nr, bool departure)
-      : station_index_(station_index),
-        train_nr_(train_nr),
-        departure_(departure) {}
+  enum class event_type { DEP, ARR };
 
-  base_event() : station_index_(0), train_nr_(0), departure_(false) {}
-
-  inline bool departure() const { return departure_; }
-  inline bool arrival() const { return !departure_; }
-  inline bool found() const { return station_index_ != 0; }
-
-  unsigned station_index_;
-  uint32_t train_nr_;
-  bool departure_;
+  trip const* trp_;
+  unsigned station_idx_;
+  event_type type_;
 };
 
-struct graph_event;
-
 struct schedule_event : public base_event {
-  schedule_event(unsigned station_index, uint32_t train_nr, bool departure,
-                 motis::time schedule_time)
-      : base_event(station_index, train_nr, departure),
-        schedule_time_(schedule_time) {}
-
-  schedule_event() : base_event(), schedule_time_(INVALID_TIME) {}
-
-  motis::time time() const { return schedule_time_; }
-
-  inline bool valid() const {
-    return schedule_time_ != INVALID_TIME && station_index_ != 0;
-  }
-
   friend bool operator==(schedule_event const& lhs, const schedule_event& rhs) {
-    return std::tie(lhs.station_index_, lhs.train_nr_, lhs.departure_,
+    return std::tie(lhs.trp_, lhs.station_idx_, lhs.type_,
                     lhs.schedule_time_) ==
-           std::tie(rhs.station_index_, rhs.train_nr_, rhs.departure_,
-                    rhs.schedule_time_);
+           std::tie(rhs.trp_, rhs.station_idx_, rhs.type_, rhs.schedule_time_);
   }
 
   friend bool operator<(schedule_event const& lhs, const schedule_event& rhs) {
-    if (lhs.schedule_time_ != rhs.schedule_time_) {
-      return lhs.schedule_time_ < rhs.schedule_time_;
-    } else {
-      return lhs.station_index_ == rhs.station_index_
-                 ? !lhs.departure_ && rhs.departure_
-                 : lhs.departure_ && !rhs.departure_;
-    }
+    return std::tie(lhs.trp_, lhs.station_idx_, lhs.type_, lhs.schedule_time_) <
+           std::tie(rhs.trp_, rhs.station_idx_, rhs.type_, rhs.schedule_time_);
   }
 
-  friend std::ostream& operator<<(std::ostream& os, const schedule_event& e) {
-    os << "<SE st=" << e.station_index_ << ", tr=" << e.train_nr_ << ", "
-       << (e.departure_ ? "dep" : "arr")
-       << ", t=" << motis::format_time(e.schedule_time_) << ">";
-    return os;
+  time schedule_time_;
+};
+
+struct graph_event : public base_event {
+  friend bool operator==(graph_event const& lhs, const graph_event& rhs) {
+    return std::tie(lhs.trp_, lhs.station_idx_, lhs.type_, lhs.current_time_) ==
+           std::tie(rhs.trp_, rhs.station_idx_, rhs.type_, rhs.current_time_);
   }
 
-  motis::time schedule_time_;
+  friend bool operator<(graph_event const& lhs, const graph_event& rhs) {
+    return std::tie(lhs.trp_, lhs.station_idx_, lhs.type_, lhs.current_time_) <
+           std::tie(rhs.trp_, rhs.station_idx_, rhs.type_, rhs.current_time_);
+  }
+
+  time current_time_;
 };
 
 }  // namespace motis
 
 namespace std {
+
 template <>
 struct hash<motis::schedule_event> {
   std::size_t operator()(motis::schedule_event const& e) const {
     std::size_t seed = 0;
-    motis::hash_combine(seed, e.station_index_);
-    motis::hash_combine(seed, e.train_nr_);
-    motis::hash_combine(seed, e.departure_);
+    motis::hash_combine(seed, e.trp_);
+    motis::hash_combine(seed, e.station_idx_);
+    motis::hash_combine(seed, e.type_);
     motis::hash_combine(seed, e.schedule_time_);
     return seed;
   }
 };
-}  // namespace std
 
-namespace motis {
-
-struct graph_event : public base_event {
-  graph_event(unsigned station_index, uint32_t train_nr, bool departure,
-              motis::time current_time, int32_t route_id)
-      : base_event(station_index, train_nr, departure),
-        current_time_(current_time),
-        route_id_(route_id) {}
-
-  explicit graph_event(schedule_event const& se)
-      : base_event(se.station_index_, se.train_nr_, se.departure_),
-        current_time_(se.schedule_time_),
-        route_id_(-1) {}
-
-  graph_event() : base_event(), current_time_(INVALID_TIME), route_id_(-1) {}
-
-  motis::time time() const { return current_time_; }
-
-  bool valid() { return current_time_ != INVALID_TIME && station_index_ != 0; }
-
-  friend bool operator==(graph_event const& lhs, const graph_event& rhs) {
-    return lhs.station_index_ == rhs.station_index_ &&
-           lhs.train_nr_ == rhs.train_nr_ && lhs.departure_ == rhs.departure_ &&
-           lhs.current_time_ == rhs.current_time_ &&
-           lhs.route_id_ == rhs.route_id_;
-  }
-
-  friend bool operator<(graph_event const& lhs, const graph_event& rhs) {
-    return lhs.current_time_ == rhs.current_time_
-               ? lhs.departure_ && !rhs.departure_
-               : lhs.current_time_ < rhs.current_time_;
-  }
-
-  friend std::ostream& operator<<(std::ostream& os, const graph_event& e) {
-    os << "<CE st=" << e.station_index_ << ", tr=" << e.train_nr_ << ", "
-       << (e.departure_ ? "dep" : "arr")
-       << ", t=" << motis::format_time(e.current_time_) << ", r=" << e.route_id_
-       << ">";
-    return os;
-  }
-
-  motis::time current_time_;
-  int32_t route_id_;
-};
-
-}  // namespace motis
-
-namespace std {
 template <>
 struct hash<motis::graph_event> {
   std::size_t operator()(motis::graph_event const& e) const {
     std::size_t seed = 0;
-    motis::hash_combine(seed, e.station_index_);
-    motis::hash_combine(seed, e.train_nr_);
-    motis::hash_combine(seed, e.departure_);
+    motis::hash_combine(seed, e.trp_);
+    motis::hash_combine(seed, e.station_idx_);
+    motis::hash_combine(seed, e.type_);
     motis::hash_combine(seed, e.current_time_);
     return seed;
   }
 };
+
 }  // namespace std
--- modules/rt/src/rt.cc
+++ modules/rt/src/rt.cc
@@ -1,100 +1,73 @@
 #include "motis/rt/rt.h"
 
-#include <algorithm>
-#include <fstream>
-#include <functional>
 #include <iostream>
 
 #include "boost/program_options.hpp"
 
-#include "motis/core/access/time_access.h"
-#include "motis/rt/handler/addition_handler.h"
-#include "motis/rt/handler/cancel_handler.h"
-#include "motis/rt/handler/connection_assessment_handler.h"
-#include "motis/rt/handler/connection_decision_handler.h"
-#include "motis/rt/handler/context.h"
-#include "motis/rt/handler/delay_handler.h"
-#include "motis/rt/handler/reroute_handler.h"
-#include "motis/rt/error.h"
+#include "motis/protocol/Message_generated.h"
 
+namespace p = std::placeholders;
+namespace po = boost::program_options;
 using namespace flatbuffers;
 using namespace motis::module;
-using namespace motis::rt::handler;
-using namespace motis::ris;
-namespace po = boost::program_options;
-namespace p = std::placeholders;
+using motis::ris::RISBatch;
 
 namespace motis {
 namespace rt {
 
+struct update {
+  update() = default;
+  update(unsigned station_id, time schedule_time)
+      : station_id_(station_id), schedule_time_(schedule_time) {}
+  unsigned station_id_;
+  time schedule_time_;
+};
+
+std::vector<update> get_updates(
+    schedule const& sched, Vector<Offset<ris::UpdatedEvent>> const* events) {
+  std::vector<update> updates;
+  for (auto const& ev : *events) {
+    updates.emplace_back(
+        get_station_node(sched, ev->base()->station_id()->str()));
+  }
+  return updates;
+}
+
+void handle_delay_message(schedule const& sched, ris::DelayMessage const* msg) {
+  auto const id = msg->trip_id();
+  auto const updates = get_updates(sched, msg->events());
+  auto const trp = get_trip(sched, id->station_id()->str(), id->service_num(),
+                            id->schedule_time());
+
+  auto lcon_idx = trp->lcon_idx_;
+  for (auto const& trp_e : *trp->edges_) {
+    auto const& e = *trp_e.get_edge();
+
+    for (auto const& upd : *msg->events()) {
+    }
+    e.m_.route_edge_.conns_[lcon_idx];
+  }
+}
+
 po::options_description rt::desc() {
-  po::options_description desc{"rt Module"};
+  po::options_description desc("RT Module");
   return desc;
 }
 
-void rt::on_msg(msg_ptr msg, sid, callback cb) {
-  auto req = msg->content<motis::ris::RISBatch const*>();
-  auto lock = synced_sched<schedule_access::RW>();
-
-  std::cout << "\n" << std::endl;
-
-  // auto const& sched = lock.sched();
-  // std::cout << "\n--" << std::endl;
-  // for (auto const& trip : sched.trip_mem) {
-  //   std::cout << sched.stations[trip->id.primary.station_id]->eva_nr << " "
-  //             << trip->id.primary.train_nr << " "
-  //             << motis_to_unixtime(sched, trip->id.primary.time) <<
-  //             std::endl;
-  // }
-
-  handler::context ctx{lock.sched(), {}};  // TODO
-  unsigned long exceptions = 0;
-
-  for (auto const& holder : *req->messages()) {
-    auto const& nested = holder->message_nested_root();
-    auto const& msg = nested->content();
-
-    try {
-      // rts_->_stats._counters.messages.increment();
-      switch (nested->content_type()) {
-        case MessageUnion_DelayMessage:
-          handle_delay(ctx, reinterpret_cast<DelayMessage const*>(msg));
-          break;
-        case MessageUnion_CancelMessage:
-          handle_cancel(ctx, reinterpret_cast<CancelMessage const*>(msg));
-          break;
-        case MessageUnion_AdditionMessage:
-          handle_addition(ctx, reinterpret_cast<AdditionMessage const*>(msg));
-          break;
-        case MessageUnion_RerouteMessage:
-          handle_reroute(ctx, reinterpret_cast<RerouteMessage const*>(msg));
-          break;
-        case MessageUnion_ConnectionDecisionMessage:
-          handle_connection_decision(
-              ctx, reinterpret_cast<ConnectionDecisionMessage const*>(msg));
-          break;
-        case MessageUnion_ConnectionAssessmentMessage:
-          handle_connection_assessment(
-              ctx, reinterpret_cast<ConnectionAssessmentMessage const*>(msg));
-          break;
-        default:
-          // rts_->_stats._counters.unknown.increment();
-          // rts_->_stats._counters.unknown.ignore();
-          break;
+void rt::init(motis::module::registry& reg) {
+  reg.subscribe("/ris/messages", [](msg_ptr const& msg) -> msg_ptr {
+    auto const& sched = get_schedule();
+    for (auto const& m : *motis_content(RISBatch, msg)->messages()) {
+      if (m->message_nested_root()->content_type() !=
+          ris::MessageUnion_DelayMessage) {
+        continue;
       }
-    } catch (...) {
-      // TODO
-      ++exceptions;
+
+      handle_delay_message(sched, reinterpret_cast<ris::DelayMessage const*>(
+                                      m->message_nested_root()));
     }
-  }
-
-  std::cout << "messages: " << req->messages()->size()
-            << ", exceptions: " << exceptions << std::endl;
-  std::cout << ctx.stats << std::endl;
-
-  last_run_stats_ = ctx.stats;
-
-  return cb({}, error::ok);
+    return nullptr;
+  });
 }
 
 }  // namespace rt
