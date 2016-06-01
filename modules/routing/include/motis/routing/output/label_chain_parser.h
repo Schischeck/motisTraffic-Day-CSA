8d2bef87cd716120bd1dbe44c72c74291b681960
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-06-01 19:20:54 +0200

    rt: fix slot

--- modules/routing/include/motis/routing/output/label_chain_parser.h
+++ modules/routing/include/motis/routing/output/label_chain_parser.h
@@ -41,7 +41,8 @@ state next_state(int s, Label const* c, Label const* n) {
     case IN_CONNECTION:
       if (c->connection_ == nullptr) {
         switch (c->node_->type()) {
-          case node_type::STATION_NODE: return AT_STATION;
+          case node_type::STATION_NODE:
+            return n && n->node_->is_station_node() ? WALK : AT_STATION;
           case node_type::FOOT_NODE: return WALK;
           case node_type::ROUTE_NODE:
             return n->node_->is_route_node() ? IN_CONNECTION_THROUGH
@@ -172,9 +173,9 @@ parse_label_chain(schedule const& sched, Label const* terminal_label) {
 
             last_con != nullptr);
 
-        transports.emplace_back(stop_index,
-                                static_cast<unsigned int>(stop_index) + 1,
-                                (*std::next(it))->now_ - current->now_, -1, 0);
+        transports.emplace_back(
+            stop_index, static_cast<unsigned int>(stop_index) + 1,
+            (*std::next(it))->now_ - current->now_, (*std::next(it))->slot_, 0);
 
         walk_arrival = (*std::next(it))->now_;
         last_con = nullptr;
