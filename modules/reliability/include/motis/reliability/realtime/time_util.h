97b8484415aa9978002653ec8ca9610e32dc1207
Mohammad H. Keyhani <keyhani@cs.tu-darmstadt.de>
2016-06-21 09:16:50 +0200

    reliability/time_util: use edge_access instead of graph_accessor.

--- modules/reliability/include/motis/reliability/realtime/time_util.h
+++ modules/reliability/include/motis/reliability/realtime/time_util.h
@@ -5,10 +5,8 @@
 #include "motis/core/schedule/event_type.h"
 #include "motis/core/schedule/schedule.h"
 #include "motis/core/schedule/time.h"
-
+#include "motis/core/access/edge_access.h"
 #include "motis/core/access/realtime_access.h"
-
-#include "motis/reliability/graph_accessor.h"
 
 namespace motis {
 namespace reliability {
@@ -18,11 +16,8 @@ inline motis::time get_scheduled_event_time(node const& route_node,
                                             light_connection const& lc,
                                             event_type type,
                                             schedule const& sched) {
-  return get_schedule_time(
-      sched, type == event_type::DEP
-                 ? graph_accessor::get_departing_route_edge(route_node)
-                 : graph_accessor::get_arriving_route_edge(route_node),
-      &lc, type);
+  return get_schedule_time(sched, get_route_edge(&route_node, &lc, type), &lc,
+                           type);
 }
 
 }  // namespace time_util
