0fa099f0c50d094e75a143a4bea510038a273c27
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-06-28 19:29:01 +0200

    rt: through train test case

--- modules/rt/test/invalid_update_test.cc
+++ modules/rt/test/invalid_update_test.cc
@@ -13,6 +13,7 @@ using namespace motis::test;
 using namespace motis::test::schedule;
 using motis::test::schedule::invalid_realtime::dataset_opt;
 using motis::test::schedule::invalid_realtime::get_trip_conflict_ris_message;
+using motis::test::schedule::invalid_realtime::get_ts_conflict_ris_message;
 
 struct rt_invalid_update_test : public motis_instance_test {
   struct stop_times {
@@ -62,4 +63,31 @@ TEST_F(rt_invalid_update_test, trip_conflict_test) {
   EXPECT_EQ(motis_time(1200), ev2["0000003"].arr_);
   EXPECT_EQ(motis_time(1210), ev2["0000003"].dep_);
   EXPECT_EQ(motis_time(1300), ev2["0000004"].arr_);
+}
+
+TEST_F(rt_invalid_update_test, ts_conflict_test) {
+  publish(get_ts_conflict_ris_message(sched()));
+  publish(make_no_msg("/ris/system_time_changed"));
+
+  auto ev1 =
+      get_trip_event_info(get_trip(sched(), "0000001", 1, unix_time(1010),
+                                   "0000005", unix_time(1400), "381"));
+  EXPECT_EQ(motis_time(1010), ev1["0000001"].dep_);
+  EXPECT_EQ(motis_time(1100), ev1["0000002"].arr_);
+  EXPECT_EQ(motis_time(1110), ev1["0000002"].dep_);
+  EXPECT_EQ(motis_time(1200), ev1["0000003"].arr_);
+  EXPECT_EQ(motis_time(1200), ev1["0000003"].dep_);
+  EXPECT_EQ(motis_time(1200), ev1["0000004"].arr_);
+  EXPECT_EQ(motis_time(1200), ev1["0000004"].dep_);
+  EXPECT_EQ(motis_time(1400), ev1["0000005"].arr_);
+
+  auto ev2 =
+      get_trip_event_info(get_trip(sched(), "0000001", 2, unix_time(1010),
+                                   "0000004", unix_time(1300), "382"));
+  EXPECT_EQ(motis_time(1010), ev2["0000001"].dep_);
+  EXPECT_EQ(motis_time(1100), ev2["0000002"].arr_);
+  EXPECT_EQ(motis_time(1110), ev2["0000002"].dep_);
+  EXPECT_EQ(motis_time(1200), ev2["0000003"].arr_);
+  EXPECT_EQ(motis_time(1200), ev2["0000003"].dep_);
+  EXPECT_EQ(motis_time(1200), ev2["0000004"].arr_);
 }
--- test/src/schedule/invalid_realtime.cc
+++ test/src/schedule/invalid_realtime.cc
@@ -50,9 +50,51 @@ void create_invalid_trip_is_msg(motis::schedule const& sched,
                         .Union()));
 }
 
+void create_invalid_ts_is_msg(motis::schedule const& sched,
+                              FlatBufferBuilder& fbb) {
+  // clang-format off
+  std::vector<Offset<UpdatedEvent>> events{
+    CreateUpdatedEvent(fbb,
+        motis::ris::CreateEvent(fbb,
+          fbb.CreateString("0000004"),
+          1,
+          fbb.CreateString("381"),
+          EventType_DEP,
+          unix_time(sched, 1310)
+        ),
+        unix_time(sched, 1200)
+    )
+  };
+  auto trip_id = motis::ris::CreateIdEvent(fbb,
+        fbb.CreateString("0000001"),
+        1,
+        unix_time(sched, 1010));
+  // clang-format on
+
+  fbb.Finish(
+      CreateMessage(fbb, MessageUnion_DelayMessage,
+                    motis::ris::CreateDelayMessage(fbb, trip_id, DelayType_Is,
+                                                   fbb.CreateVector(events))
+                        .Union()));
+}
+
 msg_ptr get_trip_conflict_ris_message(motis::schedule const& sched) {
   FlatBufferBuilder is_msg_fbb;
   create_invalid_trip_is_msg(sched, is_msg_fbb);
+
+  message_creator mc;
+  std::vector<Offset<MessageHolder>> messages{CreateMessageHolder(
+      mc,
+      mc.CreateVector(is_msg_fbb.GetBufferPointer(), is_msg_fbb.GetSize()))};
+  mc.create_and_finish(MsgContent_RISBatch,
+                       CreateRISBatch(mc, mc.CreateVector(messages)).Union(),
+                       "/ris/messages", DestinationType_Topic);
+  return make_msg(mc);
+}
+
+msg_ptr get_ts_conflict_ris_message(motis::schedule const& sched) {
+  FlatBufferBuilder is_msg_fbb;
+  create_invalid_ts_is_msg(sched, is_msg_fbb);
 
   message_creator mc;
   std::vector<Offset<MessageHolder>> messages{CreateMessageHolder(
