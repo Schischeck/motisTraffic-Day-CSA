9c9d53b32b9ac4aa763879151577d15b935b5ab0
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2016-07-20 13:05:59 +0200

    minimal intermodal module

---
+++ modules/intermodal/include/motis/intermodal/intermodal.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <string>
+
+#include "motis/module/module.h"
+
+using namespace motis::module;
+
+namespace motis {
+namespace intermodal {
+
+struct intermodal : public motis::module::module {
+public:
+  intermodal();
+  ~intermodal() override;
+
+  std::string name() const override { return "intermodal"; }
+  void init(motis::module::registry&) override;
+
+private:
+  motis::module::msg_ptr route(motis::module::msg_ptr const&);
+};
+
+}  // namespace intermodal
+}  // namespace motis
