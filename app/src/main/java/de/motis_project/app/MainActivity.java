1825f43a27502524abfffdc23ea88d5520e05c02
Simon Gündling <sguendling@algo.informatik.tu-darmstadt.de>
2016-07-24 12:01:56 +0200

    butterknife

--- app/src/main/java/de/motis_project/app/MainActivity.java
+++ app/src/main/java/de/motis_project/app/MainActivity.java
@@ -2,12 +2,24 @@ package de.motis_project.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button)
+    void toast() {
+        Toast toast = Toast.makeText(getApplicationContext(), "Hello!", Toast.LENGTH_SHORT);
+        toast.show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
