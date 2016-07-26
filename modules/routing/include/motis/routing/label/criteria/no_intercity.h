a7dddbb8f4e8753fef2336e7a8f7ac350fab0cc5
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-07-26 19:44:13 +0200

    fix formatting

--- modules/routing/include/motis/routing/label/criteria/no_intercity.h
+++ modules/routing/include/motis/routing/label/criteria/no_intercity.h
@@ -6,8 +6,8 @@ namespace routing {
 struct no_intercity_filter {
   template <typename Label>
   static bool is_filtered(Label const& l) {
-    if(l.connection_ != nullptr) {
-      return l.connection_->full_con_->clasz_ < 3;     
+    if (l.connection_ != nullptr) {
+      return l.connection_->full_con_->clasz_ < 3;
     } else {
       return false;
     }
