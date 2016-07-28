bc060db64646ff7553c699e6eca4fb2e8e2e5d59
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2016-07-28 20:51:54 +0200

    routes: remove OSM relation feature (for clean rebuild)

--- modules/routes/include/motis/routes/routes.h
+++ modules/routes/include/motis/routes/routes.h
@@ -1,12 +1,8 @@
 #pragma once
 
 #include <string>
-#include <vector>
-
-#include "parser/file.h"
 
 #include "motis/module/module.h"
-#include "motis/protocol/RoutesSections_generated.h"
 
 namespace motis {
 namespace routes {
@@ -24,12 +20,7 @@ private:
   motis::module::msg_ptr resolve_route_osrm(schedule const&, trip const*);
   motis::module::msg_ptr resolve_route_stub(schedule const&, trip const*);
 
-  motis::module::msg_ptr routes_section(motis::module::msg_ptr const&);
-  motis::module::msg_ptr all_sections(motis::module::msg_ptr const&);
-
-  parser::buffer routes_buf_;
   std::string routes_file_;
-  bool file_loaded_;
 };
 
 }  // namespace routes
