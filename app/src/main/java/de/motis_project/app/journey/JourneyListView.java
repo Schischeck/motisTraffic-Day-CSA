f57db35d3fda3ebf14b3be3273ffa2dcaaaf90b4
Simon Gündling <sguendling@algo.informatik.tu-darmstadt.de>
2016-08-07 15:26:50 +0200

    list item design

--- app/src/main/java/de/motis_project/app/journey/JourneyListView.java
+++ app/src/main/java/de/motis_project/app/journey/JourneyListView.java
@@ -1,15 +1,49 @@
 package de.motis_project.app.journey;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.view.View;
 
 import java.util.List;
 
+import de.motis_project.app.R;
+
 public class JourneyListView extends RecyclerView {
+
+    class SimpleDividerItemDecoration extends RecyclerView.ItemDecoration {
+        private Drawable divider;
+
+        public SimpleDividerItemDecoration(Context context) {
+            divider = ContextCompat.getDrawable(context, R.drawable.line_divider);
+        }
+
+        @Override
+        public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + divider.getIntrinsicHeight();
+
+                divider.setBounds(left, top, right, bottom);
+                divider.draw(c);
+            }
+        }
+    }
+
     public JourneyListView(Context context) {
         super(context);
         init();
@@ -31,6 +65,7 @@ public class JourneyListView extends RecyclerView {
         final LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         setAdapter(adapter);
         setLayoutManager(layoutManager);
+        addItemDecoration(new SimpleDividerItemDecoration(getContext()));
 
         addOnScrollListener(new InfiniteScroll(layoutManager) {
             @Override
