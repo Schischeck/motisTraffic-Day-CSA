d4c87a5af8dd981866c8d41b92f05d5df81f4c7d
Jonas Schlitzer <schlitzer@algo.informatik.tu-darmstadt.de>
2016-08-17 17:01:05 +0200

    segment & way sorting updated

--- modules/routes/include/motis/routes/prepare/osm_util.h
+++ modules/routes/include/motis/routes/prepare/osm_util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+
 #include "osmium/handler.hpp"
 #include "osmium/io/pbf_input.hpp"
 #include "osmium/io/reader_iterator.hpp"
@@ -7,29 +9,37 @@
 #include "osmium/memory/buffer.hpp"
 #include "osmium/osm.hpp"
 #include "osmium/visitor.hpp"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/writer.h"
+
+#include "motis/core/common/geo.h"
+
+using namespace rapidjson;
 
 namespace motis {
 namespace routes {
 
-template <typename F> void foreach_osm_node(std::string const &filename, F f) {
+template <typename F>
+void foreach_osm_node(std::string const& filename, F f) {
   osmium::io::Reader reader(filename, osmium::osm_entity_bits::node);
   for (auto it = std::begin(reader); it != std::end(reader); ++it) {
-    f(static_cast<osmium::Node &>(*it));
-  }
-}
-
-template <typename F> void foreach_osm_way(std::string const &filename, F f) {
-  osmium::io::Reader reader(filename, osmium::osm_entity_bits::way);
-  for (auto it = std::begin(reader); it != std::end(reader); ++it) {
-    f(static_cast<osmium::Way &>(*it));
+    f(static_cast<osmium::Node&>(*it));
   }
 }
 
 template <typename F>
-void foreach_osm_relation(std::string const &filename, F f) {
+void foreach_osm_way(std::string const& filename, F f) {
+  osmium::io::Reader reader(filename, osmium::osm_entity_bits::way);
+  for (auto it = std::begin(reader); it != std::end(reader); ++it) {
+    f(static_cast<osmium::Way&>(*it));
+  }
+}
+
+template <typename F>
+void foreach_osm_relation(std::string const& filename, F f) {
   osmium::io::Reader reader(filename, osmium::osm_entity_bits::relation);
   for (auto it = std::begin(reader); it != std::end(reader); ++it) {
-    f(static_cast<osmium::Relation &>(*it));
+    f(static_cast<osmium::Relation&>(*it));
   }
 }
 
@@ -45,6 +55,7 @@ struct osm_relation {
 
   std::vector<int64_t> nodes_;
   std::vector<int64_t> ways_;
+  bool broken = false;
   int64_t id_;
 };
 
@@ -61,56 +72,206 @@ struct way {
   int64_t id_;
   int64_t front_;
   int64_t back_;
+  osmium::Location f;
+  osmium::Location b;
 };
 
-inline std::vector<int64_t> sort_ways(std::vector<way> &ways) {
-  std::vector<int64_t> visited;
-  std::vector<int64_t> result;
-  std::vector<std::vector<way>> segments;
-  segments.push_back({ways[0]});
-  visited.push_back(ways[0].id_);
-  while (visited.size() != ways.size()) {
-    auto &currentway = segments.back();
-    auto front = currentway.front().front_;
-    auto back = currentway.back().back_;
-    std::vector<way> possible_front;
-    std::vector<way> possible_back;
-    for (auto const &w : ways) {
-      if (std::find(visited.begin(), visited.end(), w.id_) == visited.end()) {
+inline std::vector<way> expand_segment(
+    motis::routes::way const& way,
+    std::vector<motis::routes::way> const& ways) {
+  std::vector<motis::routes::way> seg = {way};
+  std::vector<int64_t> used = {way.id_};
+  while (used.size() < ways.size()) {
+    std::vector<motis::routes::way> add_front;
+    std::vector<motis::routes::way> add_back;
+    auto& front = seg.front().front_;
+    auto& back = seg.back().back_;
+    for (auto const& w : ways) {
+      if (std::find(begin(used), end(used), w.id_) == end(used)) {
         if (w.back_ == front) {
-          possible_front.push_back(w);
+          add_front.push_back(w);
+          used.push_back(w.id_);
         }
         if (w.front_ == back) {
-          possible_back.push_back(w);
+          add_back.push_back(w);
+          used.push_back(w.id_);
         }
       }
     }
-    if (possible_back.empty() && possible_front.empty()) {
-      for (auto const &w : ways) {
-        if (std::find(visited.begin(), visited.end(), w.id_) == visited.end()) {
-          segments.push_back({w});
-          visited.push_back(w.id_);
-          break;
-        }
-      }
+    if (add_back.size() > 1 || add_front.size() > 1) {
+      return {};
     }
-    if (possible_back.size() == 1) {
-      currentway.push_back(possible_back.front());
-      visited.push_back(possible_back.front().id_);
+    if (add_back.size() == 1) {
+      seg.push_back(add_back.front());
     }
-    if (possible_front.size() == 1) {
-      currentway.insert(currentway.begin(), possible_front.front());
-      visited.push_back(possible_front.front().id_);
+    if (add_front.size() == 1) {
+      seg.insert(begin(seg), add_front.front());
+    }
+    if (add_front.empty() && add_back.empty()) {
+      break;
     }
   }
-  for (auto const &s : segments) {
-    for (auto const &w : s) {
-      result.push_back(w.id_);
+  return seg;
+}
+
+inline std::vector<int64_t> sort_segments(
+    std::vector<std::vector<way>> segments) {
+  std::vector<int64_t> result;
+  auto start = segments.front();
+  bool add_front = false;
+  double max_dist = 10;
+  segments.erase(begin(segments));
+  while (!segments.empty()) {
+    auto const loc = add_front ? start.front().f : start.back().f;
+    std::vector<std::pair<std::size_t, bool>> add;
+    for (std::size_t i = 0; i < segments.size(); i++) {
+      auto const& f_dist = geo_detail::distance_in_m(
+          segments[i].front().f.lat(), segments[i].front().f.lon(), loc.lat(),
+          loc.lon());
+      auto const& b_dist = geo_detail::distance_in_m(segments[i].back().f.lat(),
+                                                     segments[i].back().f.lon(),
+                                                     loc.lat(), loc.lon());
+      if (f_dist < max_dist) {
+        add.emplace_back(i, false);
+      } else if (b_dist < max_dist) {
+        add.emplace_back(i, true);
+      }
     }
+    if (add.size() > 1 || (add_front && add.empty())) {
+      return {};
+    }
+    if (add.empty()) {
+      add_front = true;
+    } else {
+      auto s = segments[add[0].first];
+      if (add[0].second) {
+        std::reverse(begin(s), end(s));
+      }
+      if (add_front) {
+        start.insert(begin(start), begin(s), end(s));
+      } else {
+        start.insert(end(start), begin(s), end(s));
+      }
+      segments.erase(begin(segments) + add[0].first);
+    }
+  }
+  for (auto const& s : start) {
+    result.push_back(s.id_);
   }
   return result;
 }
 
+inline std::vector<int64_t> sort_ways(std::vector<way>& ways) {
+  std::vector<std::vector<way>> segments;
+  if (ways.empty()) {
+    return {};
+  }
+  while (!ways.empty()) {
+    auto& start = ways[0];
+    auto const& seg = expand_segment(start, ways);
+    if (seg.empty()) {
+      return {};
+    }
+    for (auto const& s : seg) {
+      auto w = std::find_if(begin(ways), end(ways),
+                            [s](auto&& w) { return w.id_ == s.id_; });
+      if (w != end(ways)) {
+        ways.erase(w);
+      }
+    }
+    segments.push_back(seg);
+  }
+  return sort_segments(segments);
+}
+
+inline void prepare_relations(std::map<int64_t, osm_relation>& relations,
+                              std::map<int64_t, osm_way> const& ways,
+                              std::map<int64_t, osm_node>& nodes) {
+  std::cout << std::endl;
+  std::vector<int64_t> broken_relations;
+  int count = 0;
+  for (auto& rel : relations) {
+    std::cout << count << "/" << relations.size() << "\r";
+    std::vector<way> small_ways;
+    count++;
+    for (auto const& w : rel.second.ways_) {
+      auto const way = ways.find(w);
+      if (way != ways.end() && !way->second.nodes_.empty()) {
+        small_ways.emplace_back(way->second.id_, way->second.nodes_.front(),
+                                way->second.nodes_.back());
+        auto f = nodes.find(small_ways.back().front_);
+        auto b = nodes.find(small_ways.back().back_);
+        if (f != end(nodes) && b != end(nodes)) {
+          small_ways.back().f = f->second.location_;
+          small_ways.back().b = b->second.location_;
+        }
+      }
+    }
+    auto const& sorted_ways = sort_ways(small_ways);
+    if (sorted_ways.empty()) {
+      broken_relations.push_back(rel.first);
+      rel.second.broken = true;
+      continue;
+    }
+    rel.second.ways_.clear();
+    for (auto const& w : sorted_ways) {
+      auto way = ways.find(w);
+      if (way != end(ways)) {
+        rel.second.ways_.push_back(w);
+        rel.second.nodes_.insert(rel.second.nodes_.end(),
+                                 way->second.nodes_.begin(),
+                                 way->second.nodes_.end());
+      }
+    }
+  }
+  std::cout << "Broken relations: " << broken_relations.size() << std::endl;
+}
+
+inline void export_geojson(std::vector<osm_relation> const& rels,
+                           std::map<int64_t, osm_way> const& ways,
+                           std::map<int64_t, osm_node> const& nodes) {
+  std::cout << "EXPORT: " << rels.size() << std::endl;
+  FILE* fp = std::fopen("geo.json", "w");
+  char writeBuffer[65536];
+
+  rapidjson::FileWriteStream os(fp, writeBuffer, sizeof(writeBuffer));
+  rapidjson::Writer<rapidjson::FileWriteStream> w(os);
+
+  w.StartObject();
+  w.String("type").String("FeatureCollection");
+  w.String("features").StartArray();
+
+  for (auto const& rel : rels) {
+    // for (auto const& goal : goal_idx) {
+    w.StartObject();
+    w.String("type").String("Feature");
+    w.String("properties").StartObject().EndObject();
+    w.String("geometry").StartObject();
+    w.String("type").String("LineString");
+    w.String("coordinates").StartArray();
+
+    for (auto const& way : rel.ways_) {
+      auto wy = ways.find(way);
+      if (wy != end(ways)) {
+        for (auto const& n : wy->second.nodes_) {
+          auto node = nodes.find(n);
+          if (node != end(nodes)) {
+            w.StartArray();
+            w.Double(node->second.location_.lon(), 9);
+            w.Double(node->second.location_.lat(), 9);
+            w.EndArray();
+          }
+        }
+      }
+    }
+    w.EndArray();
+    w.EndObject();
+    w.EndObject();
+  }
+
+  w.EndArray();
+  w.EndObject();
+}
 struct segment_match {
   segment_match(int64_t rel, std::vector<std::string> stations,
                 std::vector<int64_t> station_nodes)
--- modules/routes/src/prepare/relation_matcher.cc
+++ modules/routes/src/prepare/relation_matcher.cc
@@ -8,22 +8,22 @@
 namespace motis {
 namespace routes {
 
-relation_matcher::relation_matcher(motis::loader::Schedule const *sched,
-                                   std::string const &osm_file)
+relation_matcher::relation_matcher(motis::loader::Schedule const* sched,
+                                   std::string const& osm_file)
     : sched_(sched), osm_file_(osm_file) {
   load_osm();
 };
 
 void relation_matcher::load_osm() {
   logging::scoped_timer scoped_timer("Loading osm data");
-  foreach_osm_relation(osm_file_, [&](auto &&relation) {
+  foreach_osm_relation(osm_file_, [&](auto&& relation) {
     std::string const type = relation.get_value_by_key("type", "");
     std::string const route = relation.get_value_by_key("route", "");
     if ((type == "route" || type == "public_transport") &&
         (route == "subway" || route == "light_rail" || route == "railway" ||
          route == "train" || route == "tram" || route == "bus")) {
       auto r = relations_.emplace(relation.id(), osm_relation(relation.id()));
-      for (auto const &member : relation.members()) {
+      for (auto const& member : relation.members()) {
         if (member.type() == osmium::item_type::way) {
           ways_.emplace(member.ref(), osm_way(member.ref()));
           r.first->second.ways_.push_back(member.ref());
@@ -31,44 +31,36 @@ void relation_matcher::load_osm() {
       }
     }
   });
-  foreach_osm_way(osm_file_, [&](auto &&way) {
+  foreach_osm_way(osm_file_, [&](auto&& way) {
     auto w = ways_.find(way.id());
-    if (w == end(ways_)) {
+    if (w == end(ways_) ||
+        std::string(way.get_value_by_key("highway", "")) == "platform" ||
+        std::string(way.get_value_by_key("public_transport", "")) ==
+            "platform") {
       return;
     }
-    for (auto const &node : way.nodes()) {
+    for (auto const& node : way.nodes()) {
       w->second.nodes_.push_back(node.ref());
       nodes_.emplace(node.ref(), osm_node(node.ref()));
     }
   });
-  foreach_osm_node(osm_file_, [&](auto &&node) {
+  foreach_osm_node(osm_file_, [&](auto&& node) {
     auto n = nodes_.find(node.id());
     if (n != end(nodes_)) {
       n->second.location_ = node.location();
     }
   });
-  std::vector<int64_t> broken_relations;
-  for (auto &rel : relations_) {
-    std::vector<way> ways;
-    for (auto const &w : rel.second.ways_) {
-      auto const way = ways_.at(w);
-      ways.emplace_back(way.id_, way.nodes_.front(), way.nodes_.back());
-    }
-    auto const &sorted_ways = sort_ways(ways);
-    rel.second.ways_.clear();
-    for (auto const &w : sorted_ways) {
-      auto way = ways_.at(w);
-      rel.second.ways_.push_back(w);
-      rel.second.nodes_.insert(rel.second.nodes_.end(), way.nodes_.begin(),
-                               way.nodes_.end());
+  prepare_relations(relations_, ways_, nodes_);
+  std::vector<osm_relation> output;
+  for (auto const& rel : relations_) {
+    if (!rel.second.broken) {
+      output.push_back(rel.second);
     }
   }
-
+  export_geojson(output, ways_, nodes_);
   LOG(logging::log_level::info) << "Relations: " << relations_.size();
   LOG(logging::log_level::info) << "Ways: " << ways_.size();
   LOG(logging::log_level::info) << "Nodes: " << nodes_.size();
-  LOG(logging::log_level::info) << "Broken relations: "
-                                << broken_relations.size();
   LOG(logging::log_level::info) << "Routes in schedule: "
                                 << sched_->routes()->size();
 }
@@ -80,7 +72,7 @@ bool relation_matcher::nodes_in_order(segment_match sm) {
   }
   auto nodes = rel->second.nodes_;
   auto it = std::find(begin(nodes), end(nodes), sm.station_nodes_[0]);
-  for (auto const &n : sm.station_nodes_) {
+  for (auto const& n : sm.station_nodes_) {
     auto next_it = std::find(begin(nodes), end(nodes), n);
     if (next_it < it) {
       return false;
@@ -95,16 +87,16 @@ void relation_matcher::find_perfect_matches() {
   int count = 0;
   int result = 0;
   std::vector<int> matched_routes;
-  for (auto const &rel : relations_) {
+  for (auto const& rel : relations_) {
     std::vector<osmium::Location> locs;
-    for (auto const &r : rel.second.nodes_) {
+    for (auto const& r : rel.second.nodes_) {
       locs.push_back(nodes_.at(r).location_);
     }
-    auto rtree = make_point_rtree(locs, [](auto &&s) {
+    auto rtree = make_point_rtree(locs, [](auto&& s) {
       return point_rtree::point{s.lon(), s.lat()};
     });
     auto route_count = 0;
-    for (auto const &r : *sched_->routes()) {
+    for (auto const& r : *sched_->routes()) {
       std::cout << count << "/" << relations_.size() << "|" << route_count
                 << "/" << sched_->routes()->size() << "|   " << result << "\r";
       if (std::find(std::begin(matched_routes), std::end(matched_routes),
@@ -113,7 +105,7 @@ void relation_matcher::find_perfect_matches() {
         continue;
       }
       bool matched = true;
-      for (auto const &s : *r->stations()) {
+      for (auto const& s : *r->stations()) {
         if (rtree.in_radius(s->lat(), s->lng(), 100).empty()) {
           matched = false;
           break;
@@ -135,21 +127,21 @@ void relation_matcher::find_segment_matches() {
   std::cout << std::endl;
   std::vector<segment_match> segments;
   auto count = 0;
-  for (auto const &rel : relations_) {
-    std::vector<osm_node *> n;
+  for (auto const& rel : relations_) {
+    std::vector<osm_node*> n;
     std::for_each(rel.second.nodes_.begin(), rel.second.nodes_.end(),
-                  [&](auto const &r) { n.push_back(&nodes_.at(r)); });
-    auto rtree = make_point_rtree(n, [](auto &&s) {
+                  [&](auto const& r) { n.push_back(&nodes_.at(r)); });
+    auto rtree = make_point_rtree(n, [](auto&& s) {
       return point_rtree::point{s->location_.lon(), s->location_.lat()};
     });
     int route_count = 0;
-    for (auto const &r : *sched_->routes()) {
+    for (auto const& r : *sched_->routes()) {
       std::cout << count << "/" << relations_.size() << "|" << route_count++
                 << "/" << sched_->routes()->size() << "|   " << segments.size()
                 << "\r";
       std::vector<std::string> stations;
       std::vector<int64_t> station_nodes;
-      for (auto const &s : *r->stations()) {
+      for (auto const& s : *r->stations()) {
         auto result = rtree.in_radius(s->lat(), s->lng(), 100);
         if (!result.empty()) {
           station_nodes.push_back(n[result[0]]->id_);
@@ -170,5 +162,5 @@ void relation_matcher::find_segment_matches() {
   }
 }
 
-} // namespace routes
-} // namespace motis
+}  // namespace routes
+}  // namespace motis
