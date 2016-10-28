e13566d831aedad5fdc261065f6915cd88c65db5
Pablo Hoch <pablo@melaxis.com>
2016-10-28 18:02:44 +0200

    connection detail view: header with white background

--- src/Widgets/ConnectionDetails.elm
+++ src/Widgets/ConnectionDetails.elm
@@ -111,15 +111,15 @@ connectionInfoView closeMsg locale connection =
                 [ div [ onClick closeMsg, class "back" ]
                     [ i [ class "icon" ] [ text "arrow_back" ] ]
                 ]
-            , div [ class "pure-u-5-24 connection-times" ]
+            , div [ class "pure-u-4-24 connection-times" ]
                 [ div [ class "connection-departure" ]
-                    [ text (Maybe.map (formatShortDateTime locale.dateConfig) (departureTime connection) |> Maybe.withDefault "?")
+                    [ text (Maybe.map formatTime (departureTime connection) |> Maybe.withDefault "?")
                     ]
                 , div [ class "connection-arrival" ]
-                    [ text (Maybe.map (formatShortDateTime locale.dateConfig) (arrivalTime connection) |> Maybe.withDefault "?")
+                    [ text (Maybe.map formatTime (arrivalTime connection) |> Maybe.withDefault "?")
                     ]
                 ]
-            , div [ class "pure-u-16-24" ]
+            , div [ class "pure-u-17-24" ]
                 [ div [] [ text (Maybe.map (.station >> .name) (List.head connection.stops) |> Maybe.withDefault "?") ]
                 , div [] [ text (Maybe.map (.station >> .name) (last connection.stops) |> Maybe.withDefault "?") ]
                 , div [ class "summary" ]
