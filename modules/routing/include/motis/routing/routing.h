8fd999c3805fe4487a75f5059de610f472231a67
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-11-29 14:36:36 +0100

    routing: remove max_labels setting (label allocator is dynamic)

--- modules/routing/include/motis/routing/routing.h
+++ modules/routing/include/motis/routing/routing.h
@@ -13,12 +13,7 @@ struct memory;
 
 struct routing : public motis::module::module {
   routing();
-  ~routing() override;
-  std::string name() const override { return "routing"; }
-
-  boost::program_options::options_description desc() override;
-  void print(std::ostream& out) const override;
-  bool empty_config() const override { return false; }
+  ~routing();
 
   void init(motis::module::registry&) override;
 
@@ -27,7 +22,6 @@ private:
   motis::module::msg_ptr route(motis::module::msg_ptr const&);
   motis::module::msg_ptr trip_to_connection(motis::module::msg_ptr const&);
 
-  std::size_t max_label_bytes_;
   std::mutex mem_pool_mutex_;
   std::vector<std::unique_ptr<memory>> mem_pool_;
 };
