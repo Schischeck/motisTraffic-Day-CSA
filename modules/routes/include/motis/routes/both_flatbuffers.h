31b884a71b5ac50fda3367fb336cce10e066368e
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-11-29 18:58:13 +0100

    remove routes code

--- modules/routes/include/motis/routes/both_flatbuffers.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "flatbuffers/flatbuffers.h"
-#undef FLATBUFFERS_H_
-#undef FLATBUFFERS_NAMESPACE
-#define FLATBUFFERS_NAMESPACE flatbuffers64
-#include "flatbuffers/flatbuffers.h"
