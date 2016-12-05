2e74c50d7deb43cbeccb14690d9d839572c0d63d
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2016-12-05 14:32:30 +0100

    remove old web folder

--- web/index.js
+++
@@ -1,8 +0,0 @@
-var app = document.createElement('app');
-document.getElementsByTagName('body')[0].appendChild(app);
-
-if (riot.parsers) {
-  riot.parsers.css.precss = precss.precss.bind(null, colors);
-  riot.parsers.js.module = compileJsModule;
-}
-loadJsModules(() => { init(); });
