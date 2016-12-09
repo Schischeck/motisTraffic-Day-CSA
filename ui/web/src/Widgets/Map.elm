a204bb90ddd049d6aacd25202623853d39266a86
Pablo Hoch <pablo@melaxis.com>
2016-12-09 18:29:06 +0100

    web ui: handle railviz train response

--- ui/web/src/Widgets/Map.elm
+++ ui/web/src/Widgets/Map.elm
@@ -25,6 +25,8 @@ import Random
 import Bitwise
 import Maybe.Extra exposing (isJust, isNothing)
 import Util.Core exposing ((=>))
+import Util.List exposing ((!!))
+import List.Extra
 import Data.RailViz.Types exposing (..)
 import Data.RailViz.Request
 import Data.RailViz.Decode exposing (decodeRailVizTrainsResponse)
@@ -50,9 +52,12 @@ type alias Vertex =
 
 
 type alias RVTrain =
-    { currentSegment : List ( Vec2, Float )
+    { names : List String
+    , currentSegment : List ( Vec2, Float )
     , departureTime : Time
     , arrivalTime : Time
+    , scheduledDepartureTime : Time
+    , scheduledArrivalTime : Time
     , departureStation : RVStation
     , arrivalStation : RVStation
     , pathLength : Float
@@ -302,8 +307,7 @@ update msg model =
 
                 cmds =
                     if List.isEmpty model_.rvTrains then
-                        [ Task.perform GenerateDemoTrains Time.now
-                        , sendRequest model_.remoteAddress
+                        [ sendRequest model_.remoteAddress
                             { corner1 =
                                 { lat = model_.mapInfo.geoBounds.north
                                 , lng = model_.mapInfo.geoBounds.west
@@ -347,9 +351,6 @@ update msg model =
             let
                 model_ =
                     handleMapMouseUpdate mapMouseUpdate model
-
-                _ =
-                    Debug.log "MouseMove" model_.hoveredTrain
             in
                 model_ ! []
 
@@ -358,8 +359,12 @@ update msg model =
                 model_ =
                     handleMapMouseUpdate mapMouseUpdate model
 
+                selectedTrain =
+                    model_.hoveredTrain
+                        |> Maybe.andThen (\pickId -> model_.rvTrains !! pickId)
+
                 _ =
-                    Debug.log "MouseDown" model_.hoveredTrain
+                    Debug.log "MouseDown" selectedTrain
             in
                 model_ ! []
 
@@ -367,9 +372,6 @@ update msg model =
             let
                 model_ =
                     handleMapMouseUpdate mapMouseUpdate model
-
-                _ =
-                    Debug.log "MouseUp" model_.hoveredTrain
             in
                 model_ ! []
 
@@ -377,9 +379,6 @@ update msg model =
             let
                 model_ =
                     handleMapMouseUpdate mapMouseUpdate model
-
-                _ =
-                    Debug.log "MouseOut" model_.hoveredTrain
             in
                 model_ ! []
 
@@ -395,7 +394,7 @@ update msg model =
                             ++ " stations"
                         )
             in
-                model ! []
+                (handleRailVizTrainsResponse response model) ! []
 
         ReceiveError request msg_ ->
             let
@@ -412,6 +411,120 @@ handleMapMouseUpdate mapMouseUpdate model =
             toPickId mapMouseUpdate.color
     in
         { model | hoveredTrain = pickId }
+
+
+handleRailVizTrainsResponse : RailVizTrainsResponse -> Model -> Model
+handleRailVizTrainsResponse response model =
+    let
+        rvStations =
+            List.map toRVStation response.stations
+
+        getSegment routeIdx segmentIdx =
+            response.routes
+                !! routeIdx
+                |> Maybe.andThen (\r -> r.segments !! segmentIdx)
+
+        getStation id =
+            List.Extra.find (\s -> s.station.id == id) rvStations
+
+        buildTrain : RailVizTrain -> Int -> RVStation -> RVStation -> RailVizSegment -> RVTrain
+        buildTrain train pickId depStation arrStation segment =
+            let
+                rvSeg =
+                    convertSegment segment
+
+                totalLength =
+                    List.foldr (\( _, l ) s -> s + l) 0.0 rvSeg
+            in
+                { names = train.names
+                , currentSegment = rvSeg
+                , departureTime = Date.toTime train.depTime
+                , arrivalTime = Date.toTime train.arrTime
+                , scheduledDepartureTime = Date.toTime train.scheduledDepTime
+                , scheduledArrivalTime = Date.toTime train.scheduledArrTime
+                , departureStation = depStation
+                , arrivalStation = arrStation
+                , pathLength = totalLength
+                , currentSubSegment = Nothing
+                , pickId = pickId
+                , pickColor = toPickColor pickId
+                }
+
+        tryBuildTrain : Int -> RailVizTrain -> Maybe RVTrain
+        tryBuildTrain pickId train =
+            let
+                seg =
+                    getSegment train.routeIndex train.segmentIndex
+
+                depStation =
+                    seg |> Maybe.map .fromStationId |> Maybe.andThen getStation
+
+                arrStation =
+                    seg |> Maybe.map .toStationId |> Maybe.andThen getStation
+            in
+                Maybe.map3
+                    (buildTrain train pickId)
+                    depStation
+                    arrStation
+                    seg
+
+        rvTrains =
+            response.trains
+                |> List.indexedMap tryBuildTrain
+                |> List.filterMap identity
+    in
+        { model
+            | rvTrains = rvTrains
+        }
+
+
+convertSegment : RailVizSegment -> List ( Vec2, Float )
+convertSegment seg =
+    let
+        latLngToVec2 lat lng =
+            let
+                ( x, y ) =
+                    latLngToWorldCoord lat lng
+            in
+                vec2 x y
+
+        convertCoords : List Float -> Maybe Vec2
+        convertCoords l =
+            case l of
+                [ lat, lng ] ->
+                    Just (latLngToVec2 lat lng)
+
+                _ ->
+                    let
+                        _ =
+                            Debug.log "RailViz: Invalid Polyline (1)" <|
+                                seg
+                    in
+                        Nothing
+
+        coords : List Vec2
+        coords =
+            List.Extra.groupsOf 2 seg.coordinates.coordinates
+                |> List.filterMap convertCoords
+
+        toOffsets : Vec2 -> ( Vec2, List Vec2 ) -> ( Vec2, List Vec2 )
+        toOffsets pt ( last, result ) =
+            ( pt, (Vector2.sub pt last) :: result )
+    in
+        case coords of
+            firstPt :: pts ->
+                pts
+                    |> List.foldl toOffsets ( firstPt, [] )
+                    |> Tuple.second
+                    |> List.map (\v -> ( v, Vector2.length v ))
+
+            _ ->
+                let
+                    _ =
+                        Debug.log "RailViz: Invalid Polyline (2)" <|
+                            seg
+                in
+                    []
 
 
 
@@ -623,9 +736,12 @@ generateDemoTrain topLeft bottomRight currentTime seed0 pickId =
             toPickColor pickId
 
         train =
-            { currentSegment = edge
+            { names = []
+            , currentSegment = edge
             , departureTime = depTime
             , arrivalTime = arrTime
+            , scheduledDepartureTime = depTime
+            , scheduledArrivalTime = arrTime
             , departureStation = depStation
             , arrivalStation = arrStation
             , pathLength = totalLength
