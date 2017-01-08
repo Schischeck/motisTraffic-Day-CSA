810c74e06e62c43c8e0a950ed1494834a81064a1
Pablo Hoch <pablo@melaxis.com>
2017-01-08 18:14:48 +0100

    ui/web: LookupStationEventsRequest

--- ui/web/src/Data/Lookup/Request.elm
+++ ui/web/src/Data/Lookup/Request.elm
@@ -1,9 +1,11 @@
 module Data.Lookup.Request exposing (..)
 
+import Data.Lookup.Types exposing (..)
 import Json.Encode as Encode exposing (string, int)
 import Util.Core exposing ((=>))
 import Util.Date exposing (unixTime)
 import Data.Connection.Types exposing (TripId)
+import Date exposing (Date)
 
 
 encodeTripToConnection : TripId -> Encode.Value
@@ -29,3 +31,57 @@ encodeTripId tripId =
         , "target_time" => int (unixTime tripId.target_time)
         , "line_id" => string tripId.line_id
         ]
+
+
+encodeStationEventsRequest : LookupStationEventsRequest -> Encode.Value
+encodeStationEventsRequest req =
+    Encode.object
+        [ "destination"
+            => Encode.object
+                [ "type" => Encode.string "Module"
+                , "target" => Encode.string "/lookup/station_events"
+                ]
+        , "content_type" => Encode.string "LookupStationEventsRequest"
+        , "content"
+            => Encode.object
+                [ "station_id" => Encode.string req.stationId
+                , "interval"
+                    => Encode.object
+                        [ "begin" => Encode.int req.intervalStart
+                        , "end" => Encode.int req.intervalEnd
+                        ]
+                , "type" => encodeTableType req.tableType
+                ]
+        ]
+
+
+encodeTableType : TableType -> Encode.Value
+encodeTableType tt =
+    case tt of
+        ArrivalsAndDepartures ->
+            Encode.string "BOTH"
+
+        OnlyArrivals ->
+            Encode.string "ONLY_ARRIVALS"
+
+        OnlyDepartures ->
+            Encode.string "ONLY_DEPARTURES"
+
+
+initialStationEventsRequest : String -> Date -> LookupStationEventsRequest
+initialStationEventsRequest stationId date =
+    let
+        selectedTime =
+            unixTime date
+
+        startTime =
+            selectedTime - 3600
+
+        endTime =
+            selectedTime + 3600
+    in
+        { stationId = stationId
+        , intervalStart = startTime
+        , intervalEnd = endTime
+        , tableType = ArrivalsAndDepartures
+        }
