f6d31d61b782c97a6492ae4fb7440e2330baacaa
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2017-01-23 17:53:45 +0100

    path: osrm works

--- modules/path/src/prepare/strategy/osrm_strategy.cc
+++ modules/path/src/prepare/strategy/osrm_strategy.cc
@@ -6,8 +6,8 @@
 #include "osrm/route_parameters.hpp"
 
 // #include "engine/datafacade/datafacade_base.hpp"
-#include "engine/datafacade/internal_datafacade.hpp"
-#include "engine/plugins/viaroute.hpp"
+#include "engine/datafacade/internal_datafacade.hpp"  // osrm
+#include "engine/plugins/viaroute.hpp"  // osrm
 
 // #include "engine/datafacade/internal_datafacade.hpp" // osrm
 #include "engine/routing_algorithms/multi_target.hpp"  // osrm
@@ -31,6 +31,7 @@ using namespace osrm::engine;
 using namespace osrm::engine::datafacade;
 using namespace osrm::engine::plugins;
 using namespace osrm::engine::routing_algorithms;
+using namespace osrm::storage;
 using namespace osrm::util;
 using namespace osrm::util::json;
 
@@ -48,21 +49,18 @@ geo::latlng make_latlng(FloatCoordinate const& coord) {
 struct osrm_strategy::impl {
   impl(strategy_id_t const strategy_id, std::vector<station> const& stations,
        std::string const& path)
-      : strategy_id_(strategy_id) {
-
-    osrm_data_facade_ = std::make_unique<InternalDataFacade>({path});
-
-    osrm_heaps_ = std::make_unique<SearchEngineData>();
-    mt_forward_ = {osrm_data_facade_.get(), *osrm_heaps_};
-    mt_backward_ = {osrm_data_facade_.get(), *osrm_heaps_};
-
-    route_plugin_ = std::make_unique<ViaRoutePlugin>(*query_data_facade, -1);
-
+      : strategy_id_(strategy_id),
+        osrm_data_facade_(
+            std::make_unique<InternalDataFacade>(StorageConfig{path})),
+        osrm_heaps_(std::make_unique<SearchEngineData>()),
+        mt_forward_(osrm_data_facade_.get(), *osrm_heaps_),
+        mt_backward_(osrm_data_facade_.get(), *osrm_heaps_),
+        via_route_(std::make_unique<ViaRoutePlugin>(*osrm_data_facade_, -1)) {
     stations_to_nodes_.set_empty_key("");
     for (auto const& station : stations) {
       auto const nodes_with_dists =
           osrm_data_facade_->NearestPhantomNodesFromBigComponent(
-              make_fixed_coord(station.pos_), 3);
+              make_coord(station.pos_), 3);
 
       stations_to_nodes_[station.id_] =
           utl::to_vec(nodes_with_dists, [&](auto const& node_with_dist) {
@@ -77,8 +75,7 @@ struct osrm_strategy::impl {
     verify(it != end(stations_to_nodes_), "osrm: unknown station!");
 
     return utl::to_vec(it->second, [&](auto const& idx) -> node_ref {
-      return {strategy_id_, idx,
-              make_latlng(node_mem_[idx].phantom_node.location)};
+      return {strategy_id_, idx, make_latlng(node_mem_[idx].location)};
     });
   }
 
@@ -110,7 +107,7 @@ struct osrm_strategy::impl {
       return utl::to_vec(from_nodes, [&](auto const& f) {
         query_phantoms[0] = f;
 
-        auto const results = forward ? mt_backward_(query_phantoms)
+        auto const results = forward ? mt_forward_(query_phantoms)
                                      : mt_backward_(query_phantoms);
         verify(results, "osrm routing error!");
 
@@ -138,11 +135,11 @@ struct osrm_strategy::impl {
     params.geometries = RouteParameters::GeometriesType::CoordVec1D;
     params.overview = RouteParameters::OverviewType::Full;
 
-    params.coordinates.push_back(toFloating(node_mem_[from.id_]).location);
-    params.coordinates.push_back(toFloating(node_mem_[to.id_].location));
+    params.coordinates.push_back(node_mem_[from.id_].location);
+    params.coordinates.push_back(node_mem_[to.id_].location);
 
     Object result;
-    auto const status = route_plugin_->HandleRequest(params, result);
+    auto const status = via_route_->HandleRequest(params, result);
 
     if (status != Status::Ok) {
       return {};
@@ -171,7 +168,7 @@ struct osrm_strategy::impl {
   MultiTargetRouting<BaseDataFacade, true> mt_forward_;
   MultiTargetRouting<BaseDataFacade, false> mt_backward_;
 
-  std::unique_ptr<ViaRoutePlugin> route_plugin_;
+  std::unique_ptr<ViaRoutePlugin> via_route_;
 
   std::vector<PhantomNode> node_mem_;
   hash_map<std::string, std::vector<size_t>> stations_to_nodes_;
