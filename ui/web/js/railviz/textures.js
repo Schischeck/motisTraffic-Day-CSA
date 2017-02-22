9b83a6f987fb7f9d2d8f91662653f01346e229ff
Pablo Hoch <pablo@melaxis.com>
2017-02-22 12:29:04 +0100

    ui/web: railviz: train directions

--- ui/web/js/railviz/textures.js
+++ ui/web/js/railviz/textures.js
@@ -9,22 +9,50 @@ RailViz.Textures = (function() {
       var cv = document.createElement('canvas');
       cv.width = size;
       cv.height = size;
-      var ct2d = cv.getContext('2d', {alpha: true});
-      ct2d.beginPath();
-      ct2d.arc(
+      var ctx = cv.getContext('2d', {alpha: true});
+      ctx.beginPath();
+      ctx.arc(
           rad + borderThickness, rad + borderThickness, rad, 0, 2 * Math.PI,
           false);
-      ct2d.fillStyle = 'rgba(' + fillColor[0] + ',' + fillColor[1] + ',' +
+      ctx.fillStyle = 'rgba(' + fillColor[0] + ',' + fillColor[1] + ',' +
           fillColor[2] + ',' + fillColor[3] + ')';
-      ct2d.fill();
-      ct2d.lineWidth = borderThickness;
-      ct2d.strokeStyle = 'rgba(' + borderColor[0] + ',' + borderColor[1] + ',' +
+      ctx.fill();
+      ctx.lineWidth = borderThickness;
+      ctx.strokeStyle = 'rgba(' + borderColor[0] + ',' + borderColor[1] + ',' +
           borderColor[2] + ',' + borderColor[3] + ')';
-      ct2d.stroke();
-      cv.id = this.id++;
+      ctx.stroke();
       return cv;
     };
   }
 
-  return {createCircle: createCircle};
+  function createTrain() {
+    return function(size) {
+      const border = (2 / 64) * size;
+      const padding = (size - (size / 2)) / 2 + border;
+      const innerSize = size - (2 * padding);
+      const mid = size / 2;
+      var cv = document.createElement('canvas');
+      cv.width = size;
+      cv.height = size;
+      var ctx = cv.getContext('2d', {alpha: true});
+
+      ctx.beginPath();
+
+      ctx.moveTo(padding, mid - (innerSize / 4));
+      ctx.lineTo(mid, mid - (innerSize / 4));
+      ctx.lineTo(size - padding, mid);
+      ctx.lineTo(mid, mid + (innerSize / 4));
+      ctx.lineTo(padding, mid + (innerSize / 4));
+      ctx.closePath();
+
+      ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
+      ctx.fill();
+      ctx.lineWidth = border;
+      ctx.strokeStyle = 'rgba(160, 160, 160, 1.0)';
+      ctx.stroke();
+      return cv;
+    };
+  }
+
+  return {createCircle: createCircle, createTrain: createTrain};
 })();
