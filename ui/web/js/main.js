c9a533597cbce58bba78a804e6004c9be54b9446
Pablo Hoch <pablo@melaxis.com>
2017-03-01 11:42:13 +0100

    ui/web: add url query parameter for motis url
    usage:
    ?motis=host -> http://host:8080/
    ?motis=8081 -> http://<window.location.hostname>:8081/
    ?motis=host:8082 -> http://host:8082/
    ?motis=http://host:8083/foo -> http://host:8083/foo
    
    default (parameter missing): http://<window.location.hostname>:8080/
    (default port changed to 8080)

---
+++ ui/web/js/main.js
@@ -0,0 +1,80 @@
+
+function initApp() {
+  var params = getQueryParameters();
+
+  var defaultHost = window.location.hostname;
+  var defaultPort = '8080';
+  var apiEndpoint = 'http://' + defaultHost + ':' + defaultPort + '/';
+  if (params['motis']) {
+    var p = params['motis'];
+    if (/^[0-9]+$/.test(p)) {
+      apiEndpoint = 'http://' + defaultHost + ':' + p + '/';
+    } else if (!p.includes(':')) {
+      apiEndpoint = 'http://' + p + ':' + defaultPort + '/';
+    } else if (!p.startsWith('http:')) {
+      apiEndpoint = 'http://' + p;
+      if (!apiEndpoint.endsWith('/')) {
+        apiEndpoint += '/';
+      }
+    } else {
+      apiEndpoint = p;
+    }
+  }
+
+  window.app = Elm.Main.embed(
+      document.getElementById('app-container'),
+      {apiEndpoint: apiEndpoint, currentTime: Date.now()});
+
+  window.elmMaps = {};
+
+  initPorts(app, apiEndpoint);
+  handleDrop(document.getElementById('app-container'));
+}
+
+window.addEventListener('load', initApp);
+
+function handleDrop(element) {
+  element.addEventListener('drop', function(e) {
+    e.preventDefault();
+    var files = e.dataTransfer.files;
+    if (files.length == 0) {
+      return;
+    }
+    var contents = [];
+    var remaining = files.length;
+
+    function onLoad(i) {
+      return function(ev) {
+        contents[i] = ev.target.result;
+        if (--remaining == 0) {
+          var data = [];
+          for (var j = 0; j < files.length; j++) {
+            data[j] = [files[j].name, contents[j]];
+          }
+          app.ports.setRoutingResponses.send(data);
+        }
+      }
+    }
+
+    for (var i = 0; i < files.length; i++) {
+      var reader = new FileReader();
+      reader.addEventListener('load', onLoad(i));
+      reader.readAsText(files[i]);
+    }
+  });
+  element.addEventListener('dragenter', function(e) {
+    e.preventDefault();
+  });
+  element.addEventListener('dragover', function(e) {
+    e.preventDefault();
+  });
+}
+
+function getQueryParameters() {
+  var params = {};
+  window.location.search.substr(1).split('&').forEach(p => {
+    var param = p.split('=');
+    params[param[0]] = decodeURIComponent(param[1]);
+  });
+  return params;
+}
