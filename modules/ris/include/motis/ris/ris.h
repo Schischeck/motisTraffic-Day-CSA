c8c7448b9b1a1cf0618c75e2dd1e01b8cccd5935
Felix Gündling <felix.guendling@gmail.com>
2017-11-23 23:19:55 +0100

    wip

--- modules/ris/include/motis/ris/ris.h
+++ modules/ris/include/motis/ris/ris.h
@@ -1,37 +1,12 @@
 #pragma once
-
-#include <ctime>
-#include <string>
-
-#include "conf/holder.h"
+#pragma once
 
 #include "motis/module/module.h"
 
 namespace motis {
 namespace ris {
-namespace mode {
 
-struct base_mode;
-
-}  // namespace mode
-
-enum class mode_t { LIVE, SIMULATION, TEST };
-
-struct config {
-  mode_t mode_{mode_t::LIVE};
-
-  std::string input_folder_;
-  std::string database_file_;
-
-  // live mode
-  int update_interval_{0};
-  int max_days_{0};
-
-  // simulation mode
-  conf::holder<std::time_t> sim_init_time_{0};
-};
-
-struct ris final : public motis::module::module {
+struct ris : public motis::module::module {
   ris();
   ~ris() override;
 
@@ -41,12 +16,11 @@ struct ris final : public motis::module::module {
   ris(ris&&) = delete;
   ris& operator=(ris&&) = delete;
 
-  std::string name() const override { return "ris"; }
   void init(motis::module::registry&) override;
 
 private:
-  config conf_;
-  std::unique_ptr<mode::base_mode> active_mode_;
+  struct impl;
+  std::unique_ptr<impl> impl_;
 };
 
 }  // namespace ris
