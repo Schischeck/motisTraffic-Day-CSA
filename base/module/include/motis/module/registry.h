19bf360561c9533637a25953970b3d03e445c03a
Felix Gündling <guendling@algo.informatik.tu-darmstadt.de>
2018-05-04 11:49:15 +0200

    op output with timings

--- base/module/include/motis/module/registry.h
+++ base/module/include/motis/module/registry.h
@@ -2,6 +2,8 @@
 
 #include <map>
 
+#include "motis/core/common/logging.h"
+
 #include "motis/module/access_t.h"
 #include "motis/module/message.h"
 
@@ -22,32 +24,44 @@ struct registry {
   template <typename Fn>
   void register_op(std::string const& name, Fn fn,
                    access_t const access = access_t::READ) {
-    if (!operations_.emplace(name, op{std::forward<Fn>(fn), access}).second) {
+    auto const call =
+        [ fn_rec = std::forward<Fn>(fn), name ](msg_ptr const& m)->msg_ptr {
+      logging::scoped_timer t{name};
+      return fn_rec(m);
+    };
+    if (!operations_.emplace(name, op{std::move(call), access}).second) {
       throw std::runtime_error("target already registered");
     }
-  }
+  }  // namespace module
 
   template <typename Fn>
   void subscribe(std::string const& topic, Fn fn,
                  access_t const access = access_t::READ) {
-    topic_subscriptions_[topic].emplace_back(std::forward<Fn>(fn), access);
+    topic_subscriptions_[topic].emplace_back(
+        [ fn_rec = std::forward<Fn>(fn), topic ](msg_ptr const& m)->msg_ptr {
+          logging::scoped_timer t{topic};
+          return fn_rec(m);
+        },
+        access);
   }
 
   template <typename Fn>
   void subscribe_void(std::string const& topic, Fn fn,
                       access_t const access = access_t::READ) {
-    subscribe(topic,
-              [fn_rec = std::forward<Fn>(fn)](msg_ptr const&)->msg_ptr {
-                fn_rec();
-                return nullptr;
-              },
-              access);
+    subscribe(
+        topic,
+        [ fn_rec = std::forward<Fn>(fn), topic ](msg_ptr const&)->msg_ptr {
+          logging::scoped_timer t{topic};
+          fn_rec();
+          return nullptr;
+        },
+        access);
   }
 
   schedule* sched_ = nullptr;
   std::map<std::string, op> operations_;
   std::map<std::string, std::vector<op>> topic_subscriptions_;
-};
+};  // namespace motis
 
 }  // namespace module
 }  // namespace motis
